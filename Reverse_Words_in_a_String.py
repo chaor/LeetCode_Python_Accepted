d97fb4b7aced72381417adbeb6c035c24d7a82ec
chaor <chaoren.hust@gmail.com>
2014-12-23 06:45:50 -0800

    Delete Reverse_Words_in_a_String.py

--- Reverse_Words_in_a_String.py
+++
@@ -1,8 +0,0 @@
-# 2014-12-23  Runtime: 92 ms
-class Solution:
-    # @param s, a string
-    # @return a string
-    def reverseWords(self, s):
-        # if L is a list, L[::-1] can reverse it
-        # L[::-1]可以翻转L这个list
-        return ' '.join(s.split()[::-1])
