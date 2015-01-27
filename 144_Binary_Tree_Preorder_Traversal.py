dcab930db396e2d35529b2ad405c94534fa2f8e2
Chao <chaoren.hust@gmail.com>
2015-01-27 00:24:05 -0800

    144 Binary Tree Preorder Traversal

---
+++ 144_Binary_Tree_Preorder_Traversal.py
@@ -0,0 +1,25 @@
+# 2015-01-27 Runtime: 41 ms
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def preorderTraversal(self, root):
+        if not root: 
+            return []
+        else:
+            res, stack = [], [root]
+        while stack:
+            curr = stack.pop()
+            res.append(curr.val)
+            if curr.right:
+                stack.append(curr.right)
+            if curr.left:
+                stack.append(curr.left)
+        return res
