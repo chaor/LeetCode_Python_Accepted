1c6ee5c5cbb7c20adbceb2dfbc0db8e25fc6c464
Chao <chaoren.hust@gmail.com>
2015-04-14 11:12:54 -0700

    102

---
+++ 102_Binary_Tree_Level_Order_Traversal.py
@@ -0,0 +1,23 @@
+# 2015-04-14  Runtime: 63 ms
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def levelOrder(self, root):
+        self.res = []
+        self.preOrder(root, 0)
+        return self.res
+        
+    def preOrder(self, node, depth):
+        if not node: return
+        if len(self.res) == depth: self.res.append([])
+        self.res[depth].append(node.val)
+        self.preOrder(node.left, depth + 1)
+        self.preOrder(node.right, depth + 1)
