9a85899fec5d2707095fe54294a12845eb5b9637
Chao <chaoren.hust@gmail.com>
2015-05-06 09:50:11 -0700

    Add another solution for 151

--- 151_Reverse_Words_in_a_String.py
+++ 151_Reverse_Words_in_a_String.py
@@ -6,3 +6,18 @@ class Solution:
         # if L is a list, L[::-1] can reverse it
         # L[::-1]可以翻转L这个list
         return ' '.join(s.split()[::-1])
+
+
+# 2015-05-06  Runtime: 71 ms, O(n) runtime, O(n) space
+class Solution:
+    # @param s, a string
+    # @return a string
+    def reverseWords(self, s):
+        wordEndPtr, result = len(s), []
+        for i in reversed(xrange(len(s))):
+            if s[i] == ' ': 
+                wordEndPtr = i
+            elif i == 0 or s[i - 1] == ' ':
+                if result: result.append(' ')
+                result.append(s[i:wordEndPtr])
+        return ''.join(result)
