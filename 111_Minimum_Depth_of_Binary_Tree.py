41d9a2af437203668a96d97e63f41b78b48d89e6
Chao <chaoren.hust@gmail.com>
2015-05-17 15:40:02 -0700

    111

--- 111_Minimum_Depth_of_Binary_Tree.py
+++ 111_Minimum_Depth_of_Binary_Tree.py
@@ -15,3 +15,21 @@ class Solution:
         if not root.left: return self.minDepth(root.right) + 1
         if not root.right: return self.minDepth(root.left) + 1
         return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+
+############Another Solution############
+class Solution:
+    # @param {TreeNode} root
+    # @return {integer}
+    def minDepth(self, root):
+    	# level-order traversal (BFS) works well for highly unbalanced tree
+    	# It will return as long as a leaf node is reached
+        if not root: return 0
+        depth, queue, rightMost = 1, collections.deque([root]), root
+        while queue:
+            poped = queue.popleft()
+            if not (poped.left or poped.right): return depth
+            if poped.left: queue.append(poped.left)
+            if poped.right:queue.append(poped.right)
+            if poped == rightMost:
+                depth += 1
+                rightMost = poped.right if poped.right else poped.left
