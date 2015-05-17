14e0b7f5e8ca7f7a2f058aebd7d9babe408fe9be
Chao <chaoren.hust@gmail.com>
2015-05-17 11:27:03 -0700

    98

--- 98_Validate_Binary_Search_Tree.py
+++ 98_Validate_Binary_Search_Tree.py
@@ -17,3 +17,20 @@ class Solution:
         if not node: return True
         return (low == None or low < node.val) and (high == None or node.val < high) \
             and self.check(node.left, low, node.val) and self.check(node.right, node.val, high)
+
+################Another Solution###################
+class Solution:
+    # @param root, a tree node
+    # @return a boolean
+    def isValidBST(self, root):
+        # in-order traversal, check whether we got a strict monotonic increasing sequence
+        self.prev = None
+        return self.isMonotonicIncreasing(root)
+        
+    def isMonotonicIncreasing(self, p):
+        if not p: return True
+        if self.isMonotonicIncreasing(p.left):
+            if self.prev and self.prev.val >= p.val: return False
+            self.prev = p
+            return self.isMonotonicIncreasing(p.right)
+        return False
