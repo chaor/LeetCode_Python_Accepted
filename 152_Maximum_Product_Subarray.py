84ed5d620d9007768f731a741cf30d9653b3c9e6
Chao <chaoren.hust@gmail.com>
2015-05-27 09:11:56 -0700

    52

--- 152_Maximum_Product_Subarray.py
+++ 152_Maximum_Product_Subarray.py
@@ -1,16 +1,12 @@
-# 2014-12-23  Runtime: 196 ms
+# 2015-05-27  Runtime: 60 ms
 class Solution:
-    # @param A, a list of integers
-    # @return an integer
-    def maxProduct(self, A):
-        # dynamic programming, let dp[i] be the (max product, min product) of subarray ends in A[i]
-        # we need min product each step because of +/- sign
-        # 动态规划，让dp[i]表示以A[i]结尾的子数组的(最大乘积,最小乘积)
-        # 由于正负号的问题，我们也需要记录每一次的最小乘积
-        lenA, res = len(A), A[0]
-        dpMax, dpMin = [A[0] for i in xrange(lenA)], [A[0] for i in xrange(lenA)]
-        for i in xrange(1, lenA):
-            t = (A[i], dpMax[i - 1] * A[i], dpMin[i - 1] * A[i])
-            dpMax[i], dpMin[i] = max(t), min(t)
-            res = max(res, dpMax[i])
-        return res
+    # @param {integer[]} nums
+    # @return {integer}
+    def maxProduct(self, nums):
+        # let fk = maxProduct whose end index is k, gk = minProduct whose end index is k
+        # if current element > 0, fk * current will be next maxProduct, otherwise gk * current will be next maxProduct
+        Max, fk, gk = nums[0], nums[0], nums[0]
+        for i in xrange(1, len(nums)):
+            fk, gk = max(fk * nums[i], nums[i], gk * nums[i]), min(fk * nums[i], nums[i], gk * nums[i])
+            Max = max(Max, fk)
+        return Max
