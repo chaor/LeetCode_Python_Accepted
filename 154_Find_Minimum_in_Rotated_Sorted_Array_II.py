006f84498771c80a2967e82a78217ce9aed62587
Chao <chaoren.hust@gmail.com>
2015-05-29 10:13:00 -0700

    154

--- 154_Find_Minimum_in_Rotated_Sorted_Array_II.py
+++ 154_Find_Minimum_in_Rotated_Sorted_Array_II.py
@@ -1,18 +1,15 @@
-# 2014-12-23  Runtime: 232 ms
+# 2015-05-29  Runtime: 68 ms
 class Solution:
-    # @param num, a list of integer
-    # @return an integer
-    def findMin(self, num):
-        left, right = 0, len(num) - 1
-        while True:
-            if right - left <= 1:
-                return min(num[left], num[right])
-            mid = (left + right) / 2
-            if num[right] == num[mid]: # e.g. [3, 3, 1, 3]
-                right -= 1
-            elif num[left] == num[mid]:
-                left += 1
-            elif num[right] < num[mid]: # pivot is in the right half, 转折点在右半部分
-                left = mid
-            else: # pivot is in the left half, 转正点在左半部分
-                right = mid
+    # @param {integer[]} nums
+    # @return {integer}
+    def findMin(self, nums):
+        L, R = 0, len(nums) - 1
+        while L < R and nums[L] >= nums[R]:
+            M = (L + R) / 2
+            if nums[M] > nums[R]:
+                L = M + 1
+            elif nums[M] < nums[L]:
+                R = M
+            else: # nums[L] == nums[M] == nums[R], can't discard either subarrays, e.g. [1, 1, 1, 0, 1], [1, 0, 1, 1, 1]
+                L += 1
+        return nums[L]
