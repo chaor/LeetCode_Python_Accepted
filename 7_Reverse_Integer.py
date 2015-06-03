31ae6a681c02223e3feada202c494ced0ee83e28
Chao <chaoren.hust@gmail.com>
2015-06-02 21:23:54 -0700

    7

--- 7_Reverse_Integer.py
+++ 7_Reverse_Integer.py
@@ -1,11 +1,11 @@
-# 2015-05-30  Runtime: 56 ms
+# 2015-05-11  Runtime: 121 ms
 class Solution:
-    # @param {integer} x 
+    # @param {integer} x
     # @return {integer}
     def reverse(self, x):
         sign = -1 if x < 0 else 1
         ret, x = 0, abs(x)
-        while x:
+        while x != 0:
             # handle overflow/underflow
             if ret > 214748364: return 0
             ret = ret * 10 + x % 10
