552bb8cba5e4299984c3687d2c37dc426ec2126f
Chao <chaoren.hust@gmail.com>
2015-06-08 09:56:16 -0700

    88

---
+++ 88_Merge_Sorted_Array.py
@@ -0,0 +1,20 @@
+# 2015-06-08  Runtime: 56 ms
+class Solution:
+    # @param {integer[]} nums1
+    # @param {integer} m
+    # @param {integer[]} nums2
+    # @param {integer} n
+    # @return {void} Do not return anything, modify nums1 in-place instead.
+    def merge(self, nums1, m, nums2, n):
+        i, j, k = m - 1, n - 1, m + n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        while j >= 0:
+            nums1[j] = nums2[j]
+            j -= 1
