f983a9faa2d1328b205bf8fff6fa0ebd2824cef5
Chao <chaoren.hust@gmail.com>
2015-06-10 23:19:59 -0700

    225

--- 225_Implement_Stack_using_Queues.py
+++ 225_Implement_Stack_using_Queues.py
@@ -2,22 +2,20 @@
 class Stack:
     # initialize your data structure here.
     def __init__(self):
-        # Q1 will always keep all elements
-        self.Q1, self.Q2, self.stackTop = collections.deque(), collections.deque(), None
+        self.Q, self.stackTop = collections.deque(), None
         
     # @param x, an integer
     # @return nothing
     def push(self, x):
-        self.Q1.append(x)
+        self.Q.append(x)
         self.stackTop = x
 
     # @return nothing
     def pop(self):
-        for i in xrange(len(self.Q1) - 1):
-            self.stackTop = self.Q1.popleft()
-            self.Q2.append(self.stackTop)
-        self.Q1.popleft()
-        self.Q1, self.Q2 = self.Q2, self.Q1
+        for i in xrange(len(self.Q) - 1):
+            self.stackTop = self.Q.popleft()
+            self.Q.append(self.stackTop)
+        self.Q.popleft()
 
     # @return an integer
     def top(self):
@@ -25,4 +23,4 @@ class Stack:
 
     # @return an boolean
     def empty(self):
-        return not self.Q1
+        return not self.Q
