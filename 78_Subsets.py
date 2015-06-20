4080155412ed3457a970a5cda931f8222c6ba968
Chao <chaoren.hust@gmail.com>
2015-06-19 23:23:26 -0700

    78

--- 78_Subsets.py
+++ 78_Subsets.py
@@ -1,15 +1,16 @@
-# 2015-06-19  Runtime: 72 ms
+# 2015-06-19  Runtime: 68 ms
 class Solution:
     # @param {integer[]} nums
     # @return {integer[][]}
     def subsets(self, nums):
         if not nums: return []
-        # use binary, for [1,2,3], '010' means [2], '101' means [1,3]
         res = []
+        nums.sort()
+        # use binary, for [1,2,3], '010' means [2], '101' means [1,3]
         for i in xrange(2 ** len(nums)):
             # for binary, don't forget to add leading zeros to make sure it has len(nums) digits
             binary, subset = bin(i)[2:].zfill(len(nums)), []
             for j in xrange(len(nums)):
                 if binary[j] == '1': subset.append(nums[j])
-            res.append(sorted(subset))
+            res.append(subset)
         return res
