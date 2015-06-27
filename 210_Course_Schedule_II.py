f09c1f86f0ba53147ec38008263a91f63cad0b66
Chao <chaoren.hust@gmail.com>
2015-06-27 12:10:52 -0700

    210

---
+++ 210_Course_Schedule_II.py
@@ -0,0 +1,24 @@
+# 2015-06-27  Runtime: 84 ms
+class Solution:
+    # @param {integer} numCourses
+    # @param {integer[][]} prerequisites
+    # @return {integer[]}
+    def findOrder(self, numCourses, prerequisites):
+        # thanks to https://leetcode.com/discuss/35605/two-ac-solution-in-java-using-bfs-and-dfs-with-explanation
+        # initialize the graph
+        incomingEdgeCount, outgoingEdge = [0 for i in xrange(numCourses)], [[] for i in xrange(numCourses)]
+        for p in prerequisites:
+            outgoingEdge[p[1]].append(p[0])
+            incomingEdgeCount[p[0]] += 1
+        
+        # BFS, if a node's incomingEdgeCount == 0, then visit it and delete it from graph
+        order, queue = [], collections.deque()
+        for i in xrange(numCourses):
+            if incomingEdgeCount[i] == 0: queue.append(i)
+        while queue:
+            visitedCourse = queue.popleft()
+            order.append(visitedCourse)
+            for to in outgoingEdge[visitedCourse]:
+                incomingEdgeCount[to] -= 1
+                if incomingEdgeCount[to] == 0: queue.append(to)
+        return [] if len(order) != numCourses else order
