8cbc42ee51cc08de5e5a60deba7d8c0857e8f512
Chao <chaoren.hust@gmail.com>
2015-06-27 12:15:22 -0700

    207

--- 207_Course_Schedule.py
+++ 207_Course_Schedule.py
@@ -1,26 +1,24 @@
-# 2015-05-09  Runtime: 88 ms
+# 2015-06-27  Runtime: 56 ms
 class Solution:
     # @param {integer} numCourses
     # @param {integer[][]} prerequisites
     # @return {boolean}
     def canFinish(self, numCourses, prerequisites):
-        # build graph
-        self.G = [{'status':'untouched', 'neighbors':[]} for i in xrange(numCourses)]
-        for edge in prerequisites:
-            self.G[edge[1]]['neighbors'].append(edge[0])
+        # thanks to https://leetcode.com/discuss/35605/two-ac-solution-in-java-using-bfs-and-dfs-with-explanation
+        # initialize the graph
+        incomingEdgeCount, outgoingEdge = [0 for i in xrange(numCourses)], [[] for i in xrange(numCourses)]
+        for p in prerequisites:
+            outgoingEdge[p[1]].append(p[0])
+            incomingEdgeCount[p[0]] += 1
         
-        # DFS, use 'untouched', 'visiting', 'visited' to mark nodes,
-        # during DFS if a 'visiting' node is reached, then there's a cycle
-        # 碰到了一个‘visiting’状态的节点，说明有环
-        for node in xrange(numCourses):
-            if self.G[node]['status'] == 'visiting': return False
-            if self.G[node]['status'] == 'untouched' and not self.dfs(node): return False
-        return True
-    
-    def dfs(self, node):
-        self.G[node]['status'] = 'visiting'
-        for neighbor in self.G[node]['neighbors']:
-            if self.G[neighbor]['status'] == 'visiting': return False
-            if self.G[neighbor]['status'] == 'untouched' and not self.dfs(neighbor): return False 
-        self.G[node]['status'] = 'visited'
-        return True
+        # BFS, if a node's incomingEdgeCount == 0, then visit it and delete it from graph
+        order, queue = [], collections.deque()
+        for i in xrange(numCourses):
+            if incomingEdgeCount[i] == 0: queue.append(i)
+        while queue:
+            visitedCourse = queue.popleft()
+            order.append(visitedCourse)
+            for to in outgoingEdge[visitedCourse]:
+                incomingEdgeCount[to] -= 1
+                if incomingEdgeCount[to] == 0: queue.append(to)
+        return True if len(order) == numCourses else False
