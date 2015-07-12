9c733824556904c018f1218e461babaf9fc9c52c
Chao <chaoren.hust@gmail.com>
2015-07-12 14:40:17 -0700

    236

---
+++ 236_Lowest_Common_Ancestor_of_a_Binary_Tree.py
@@ -0,0 +1,20 @@
+# 2015-07-12  Runtime: 324 ms
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @param {TreeNode} p
+    # @param {TreeNode} q
+    # @return {TreeNode}
+    def lowestCommonAncestor(self, root, p, q):
+        # thanks to https://leetcode.com/discuss/45386/4-lines-c-python-ruby
+        if root in (p, q, None): return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        return root if left and right else left or right
