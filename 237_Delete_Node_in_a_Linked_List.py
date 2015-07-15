ce3c58578678f13ad49cb4cf3630d5de27af9926
Chao <chaoren.hust@gmail.com>
2015-07-14 18:22:36 -0700

    237

---
+++ 237_Delete_Node_in_a_Linked_List.py
@@ -0,0 +1,13 @@
+# 2015-07-14  Runtime: 64 ms
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} node
+    # @return {void} Do not return anything, modify node in-place instead.
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
