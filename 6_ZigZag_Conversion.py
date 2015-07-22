e33ed04baea5287e28e6793844500828ba741dc2
Chao <chaoren.hust@gmail.com>
2015-07-21 23:55:09 -0700

    6

--- 6_ZigZag_Conversion.py
+++ 6_ZigZag_Conversion.py
@@ -1,25 +1,24 @@
-# 2015-05-30  Runtime: 124 ms
+# 2015-07-21  Runtime: 148 ms
 class Solution:
     # @param {string} s
     # @param {integer} numRows
     # @return {string}
     def convert(self, s, numRows):
-        if numRows == 1: return s
-        # 把abcd...xyz写成W型就容易找到规律了，每行有两个gap
-        # 分别是2(n-1)和2x0, 2(n-2)和2x1, 2(n-3)和2x2,...2x0和2(n-1)
-        result, lineStart = [], -1
+        # thanks to https://leetcode.com/discuss/10493/easy-to-understand-java-solution
+        line = [[] for i in xrange(numRows)]
+        i = 0
+        while i < len(s):
+            j = 0  # go down
+            while j < numRows and i < len(s):
+                line[j].append(s[i])
+                i += 1
+                j += 1
+            j = numRows - 2  # go up
+            while j >= 1 and i < len(s):
+                line[j].append(s[i])
+                i += 1
+                j -= 1
+        result = ''
         for i in xrange(numRows):
-            lineStart += 1
-            if lineStart >= len(s): break
-            result.append(s[lineStart])
-            k, gap1, gap2 = lineStart, 2 * (numRows - 1 - i), 2 * i
-            while True:
-                if gap1 > 0:
-                    k += gap1
-                    if k >= len(s): break
-                    result.append(s[k])
-                if gap2 > 0:
-                    k += gap2
-                    if k >= len(s): break
-                    result.append(s[k])
-        return ''.join(result)
+            result += ''.join(line[i])
+        return result
