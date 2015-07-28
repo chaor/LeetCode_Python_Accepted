aa346510a1d108cf0a5bfd8131f8c13a3aba54e9
Chao <chaoren.hust@gmail.com>
2015-07-28 09:52:57 -0700

    15

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -1,21 +1,26 @@
-# 2015-05-16  Runtime: 631 ms
+# 2015-07-28  Runtime: 308 ms
 
 class Solution:
     # @param {integer[]} nums
     # @return {integer[][]}
     def threeSum(self, nums):
-        if len(nums) < 3: return []
+    	# thanks to https://leetcode.com/discuss/23638/concise-o-n-2-java-solution
         nums.sort()
-        result = set()
-        for i in xrange(len(nums)):
-            start, end = 0, len(nums) - 1
-            while start < end:
-                Sum = nums[i] + nums[start] + nums[end]
-                if Sum == 0:
-                    if start != i and end != i: result.add(tuple(sorted([nums[i], nums[start], nums[end]])))
-                    start, end = start + 1, end - 1
-                elif Sum < 0:
-                    start += 1
-                else:
-                    end -= 1
-        return [list(abc) for abc in result]
+        result = []
+        # ia, ib, ic is the index of the a,b,c, a <= b <= c
+        for ia in xrange(len(nums) - 2):
+            # remove duplicated a
+            if ia == 0 or (ia > 0 and nums[ia] != nums[ia - 1]):
+                ib, ic = ia + 1, len(nums) - 1
+                while ib < ic:
+                    if nums[ia] + nums[ib] + nums[ic] == 0:
+                        result.append([nums[ia], nums[ib], nums[ic]])
+                        # remove duplicated b, c
+                        while ib < ic and nums[ib] == nums[ib + 1]: ib += 1
+                        while ib < ic and nums[ic] == nums[ic - 1]: ic -= 1
+                        ib, ic = ib + 1, ic - 1
+                    elif nums[ia] + nums[ib] + nums[ic] < 0:
+                        ib += 1
+                    else:
+                        ic -= 1
+        return result
