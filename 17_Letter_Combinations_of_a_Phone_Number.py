b1e4ac7c9218bb242b315b502aa542cd0b856723
Chao <chaoren.hust@gmail.com>
2015-07-29 08:47:17 -0700

    17

--- 17_Letter_Combinations_of_a_Phone_Number.py
+++ 17_Letter_Combinations_of_a_Phone_Number.py
@@ -1,17 +1,12 @@
-# 2015-06-02  Runtime: 44 ms
+# 2015-07-29  Runtime: 44 ms
 class Solution:
     # @param {string} digits
     # @return {string[]}
     def letterCombinations(self, digits):
         if not digits: return []
-        self.d = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
-        self.digits, self.result = digits, []
-        self.dfs(0, [])
-        return self.result
-        
-    def dfs(self, i, L):
-        if i == len(self.digits):
-            self.result.append(''.join(L))
-            return
-        for letter in self.d[self.digits[i]]:
-            self.dfs(i + 1, L + [letter])
+        key = {'0':' ', '2':'abc', '3':'def', '4':'ghi',
+               '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
+        result = list(key[digits[0]])
+        for i in xrange(1, len(digits)):
+            result = [x + y for x in result for y in key[digits[i]]]
+        return result  
