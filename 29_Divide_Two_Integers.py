1b605bf583175c96d8f6d4c6baccbddf76347cd2
Chao <chaoren.hust@gmail.com>
2015-08-02 10:14:35 -0700

    29

--- 29_Divide_Two_Integers.py
+++ 29_Divide_Two_Integers.py
@@ -1,17 +1,17 @@
-# 2015-06-07  Runtime: 60 ms
+# 2015-08-02  Runtime: 56 ms
 class Solution:
     # @param {integer} dividend
     # @param {integer} divisor
     # @return {integer}
     def divide(self, dividend, divisor):
-        sign = 1 if dividend > 0 and divisor > 0 or (dividend < 0 and divisor < 0) else -1
-        dividend, divisor, quotient = abs(dividend), abs(divisor), 0
-        while dividend >= divisor:
-            n, divisorMultiple = 0, divisor
-            while dividend >= divisorMultiple:
-                dividend -= divisorMultiple
-                quotient += 1 << n
-                if quotient > 2147483647: return -2147483648 if sign < 0 else 2147483647
-                divisorMultiple <<= 1
-                n += 1
-        return quotient * sign
+        sign = 1 if (dividend > 0 and divisor > 0) or (dividend < 0 and divisor < 0) else -1
+        a, b, result = abs(dividend), abs(divisor), 0
+        while a >= b:
+            n, nb = 1, b
+            while a >= nb:
+                a -= nb
+                result += n
+                if result >= 2147483648 and sign == -1: return -2147483648
+                if result >= 2147483647 and sign == 1: return 2147483647
+                n, nb = n << 1, nb << 1
+        return sign * result
