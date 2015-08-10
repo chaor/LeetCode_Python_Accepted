7724a828c547837dbe33f9abb5a1dca7bc0b4eb3
Chao <chaoren.hust@gmail.com>
2015-08-09 23:57:56 -0700

    32

--- 32_Longest_Valid_parentheses.py
+++ 32_Longest_Valid_parentheses.py
@@ -1,14 +1,14 @@
-# 2015-06-08  Runtime: 88 ms
+# 2015-08-09  Runtime: 84 ms
 class Solution:
     # @param {string} s
     # @return {integer}
     def longestValidParentheses(self, s):
-        # 记录左括号的index，每次出现右括号且可以跟栈顶左括号匹配时，更新一下maxLen
-        stack, maxLen = [-1], 0
+        stack, length = [(')', -1)], 0
         for i in xrange(len(s)):
-            if s[i] == ')' and stack[-1] != -1 and s[stack[-1]] == '(':
+            if s[i] == ')' and stack[-1][0] == '(':
                 stack.pop()
-                maxLen = max(maxLen, i - stack[-1])
+                if i - stack[-1][1] > length:
+                    length = i - stack[-1][1]
             else:
-                stack.append(i)
-        return maxLen
+                stack.append((s[i], i))
+        return length
