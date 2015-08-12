73687ebff644decc677e00ac2d2c8ce415918765
Chao <chaoren.hust@gmail.com>
2015-08-12 16:14:56 -0700

    36

--- 36_Valid_Sudoku.py
+++ 36_Valid_Sudoku.py
@@ -1,18 +1,29 @@
-# 2015-06-13  Runtime: 88 ms
+# 2015-08-12  Runtime: 68 ms
 class Solution:
     # @param {character[][]} board
     # @return {boolean}
     def isValidSudoku(self, board):
-        # validate rows
+        # check rows
+        for row in board:
+            s = set()
+            for char in row:
+                if char == '.': continue
+                if char in s: return False
+                s.add(char)
+        # check columns
         for i in xrange(9):
-            if board[i].count('.') + len(set(board[i])) - 1 != 9: return False
-        # validate columns
-        for i in xrange(9):
-            col = [board[j][i] for j in xrange(9)]
-            if col.count('.') + len(set(col)) - 1 != 9: return False
-        # validate 3x3 squares
+            s = set()
+            for j in xrange(9):
+                if board[j][i] == '.': continue
+                if board[j][i] in s: return False
+                s.add(board[j][i])
+        # check 3x3 squares
         for i in (0, 3, 6):
             for j in (0, 3, 6):
-                square = [board[i + m][j + n] for m in (0, 1, 2) for n in (0, 1, 2)]
-                if square.count('.') + len(set(square)) - 1 != 9: return False
+                s = set()
+                for m in (0, 1, 2):
+                    for n in (0, 1, 2):
+                        if board[i + m][j + n] == '.': continue
+                        if board[i + m][j + n] in s: return False
+                        s.add(board[i + m][j + n])
         return True
