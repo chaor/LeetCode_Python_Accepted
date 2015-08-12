7010b52abf36cef4e4ea08ec975d1f4609251cca
Chao <chaoren.hust@gmail.com>
2015-08-12 08:50:07 -0700

    34

--- 34_Search_for_a_Range.py
+++ 34_Search_for_a_Range.py
@@ -1,22 +1,27 @@
-# 2015-06-13  Runtime: 52 ms
+# 2015-08-12  Runtime: 52 ms
 class Solution:
     # @param {integer[]} nums
     # @param {integer} target
     # @return {integer[]}
     def searchRange(self, nums, target):
-        self.nums, self.target = nums, target
-        return self.search(0, len(self.nums) - 1)
-        
-    def search(self, L, R):
-        if L == R:
-            return [L, L] if self.nums[L] == self.target else [-1, -1]
-        while L <= R:
-            M = (L + R) / 2
-            if self.target > self.nums[M]:
+        # thanks to https://leetcode.com/discuss/18242/clean-iterative-solution-binary-searches-with-explanation
+        L, R, result = 0, len(nums) - 1, [-1, -1]
+        # search for the left one
+        while L < R:
+            M = (L + R) / 2 # biased to left
+            if nums[M] < target:
                 L = M + 1
-            elif self.target < self.nums[M]:
+            else:
+                R = M
+        if nums[L] != target:
+            return result
+        result[0] = L
+        R = len(nums) - 1
+        while L < R:
+            M = (L + R) / 2 + 1 # biased to right
+            if nums[M] > target:
                 R = M - 1
-            else: # self.target == self.nums[M]
-                left, right = self.search(L, M - 1), self.search(M + 1, R)
-                return [M if left[0] == -1 else left[0], M if right[1] == -1 else right[1]]
-        return [-1, -1]
+            else:
+                L = M
+        result[1] = R
+        return result
