9d122109a38baed8809fafb2634ba7b64b19b889
Chao <chaoren.hust@gmail.com>
2015-08-12 09:02:45 -0700

    35

--- 35_Search_Insert_Position.py
+++ 35_Search_Insert_Position.py
@@ -1,15 +1,16 @@
-# 2015-05-28  Runtime: 60 ms
+# 2015-08-12  Runtime: 44 ms
 class Solution:
     # @param {integer[]} nums
     # @param {integer} target
     # @return {integer}
     def searchInsert(self, nums, target):
         L, R = 0, len(nums) - 1
-        while L < R:
+        while L <= R:
             M = (L + R) / 2
-            if target > nums[M]:
-                L = M + 1  # index 0, 1, ... M can be excluded
+            if nums[M] == target:
+                return M
+            if nums[M] < target:
+                L = M + 1
             else:
-                R = M  # index M + 1, M + 2, ... len(nums) - 1 can be excluded
-        # finally L == R
-        return L if target <= nums[L] else L + 1
+                R = M - 1
+        return L
