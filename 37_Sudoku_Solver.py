c5f19746427d0586c9b7130314ec8dcbe8b8c485
Chao <chaoren.hust@gmail.com>
2015-08-12 18:13:31 -0700

    37

--- 37_Sudoku_Solver.py
+++ 37_Sudoku_Solver.py
@@ -1,33 +1,32 @@
-# 2015-06-14  Runtime: 836 ms
+# 2015-08-12  Runtime: 1188 ms
 class Solution:
     # @param {character[][]} board
     # @return {void} Do not return anything, modify board in-place instead.
     def solveSudoku(self, board):
-        self.board = board
-        self.solve()
-    
-    # @return {boolean} True if a valid solution is found    
-    def solve(self):
+        self.b = board
+        self.dfs()
+
+    def dfs(self):
         for i in xrange(9):
             for j in xrange(9):
-                if self.board[i][j] == '.':
-                    for char in self.getFitCharList(i, j):
-                        self.board[i][j] = char
-                        if self.solve(): return True
-                        self.board[i][j] = '.'
+                if self.b[i][j] == '.':
+                    s = set(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+                    for m in xrange(9):
+                        if self.b[i][m] in s:
+                            s.remove(self.b[i][m])
+                        if self.b[m][j] in s:
+                            s.remove(self.b[m][j])
+                    for m in (0, 1, 2):
+                        for n in (0, 1, 2):
+                            if self.b[i / 3 * 3 + m][j / 3 * 3 + n] in s:
+                                s.remove(self.b[i / 3 * 3 + m][j / 3 * 3 + n])
+                    for char in s:
+                        l = [c for c in self.b[i]]
+                        l[j] = char
+                        self.b[i] = ''.join(l) 
+                        if self.dfs(): return True
+                        l = [c for c in self.b[i]]
+                        l[j] = '.'
+                        self.b[i] = ''.join(l)
                     return False
         return True
-    
-    # @param {int} i
-    # @param {int} j
-    # @return {List} a list of available characters for board[i][j]
-    def getFitCharList(self, m, n):
-        L = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-        for char in self.board[m]:
-            if char != '.': L.remove(char)
-        for i in xrange(9):
-            if self.board[i][n] != '.' and self.board[i][n] in L: L.remove(self.board[i][n])
-        for i in (m / 3 * 3, m / 3 * 3 + 1, m / 3 * 3 + 2):
-            for j in (n / 3 * 3, n / 3 * 3 + 1, n / 3 * 3 + 2):
-                if self.board[i][j] != '.'and self.board[i][j] in L: L.remove(self.board[i][j])
-        return L
