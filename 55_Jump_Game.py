4d10ebddfcd5721bf45253947133666e80f9785a
Chao <chaoren.hust@gmail.com>
2015-08-16 14:26:54 -0700

    55

--- 55_Jump_Game.py
+++ 55_Jump_Game.py
@@ -1,12 +1,10 @@
-# 2015-06-15  Runtime: 92 ms
+# 2015-08-16  Runtime: 80 ms
 class Solution:
     # @param {integer[]} nums
     # @return {boolean}
     def canJump(self, nums):
         maxReach = 0
         for i in xrange(len(nums)):
-            if maxReach >= len(nums) - 1: return True
-            if i <= maxReach:
-                maxReach = max(maxReach, i + nums[i])
-            else:
-                return False
+            if i > maxReach: return False
+            if i + nums[i] > maxReach: maxReach = i + nums[i]
+        return True
