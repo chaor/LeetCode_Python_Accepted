0e803ed0a7643003a4abe2cfc9b2880727a14f4f
Chao <chaoren.hust@gmail.com>
2015-08-23 11:39:53 -0700

    171

--- 171_Excel_Sheet_Column_Number.py
+++ 171_Excel_Sheet_Column_Number.py
@@ -1,18 +1,12 @@
-2015-01-08  Runtime: 137 ms
-
-class Solution:
-    # @param s, a string
-    # @return an integer
+# 2015-08-23   Runtime: 68 ms
+class Solution(object):
     def titleToNumber(self, s):
-        res, len_s = 0, len(s)
-        # e.g. if len_s == 5, we need to count in previous 26, 26^2, 26^3, 26^4
-        # 如果s长度为5，那我们需要先加上之前的26, 26^2, 26^3, 26^4
-        for i in xrange(1, len_s):
-            res += 26 ** i
-        # find out the position of s in AAA...A ~ ZZZ...Z (len_s digits)
-        # 找到AAA...A~ZZZ...Z(有len_s位)中s的位置
-        base = 1
-        for one_letter in reversed(s):
-            res += (ord(one_letter) - ord('A')) * base
+        """
+        :type s: str
+        :rtype: int
+        """
+        res, base = 0, 1
+        for ch in s[::-1]:
+            res += base * ((ord(ch) - ord('A')) + 1)
             base *= 26
-        return res + 1
+        return res
