cd70e197c69ca1762e945d560bf642ab13e08ecc
Chao <chaoren.hust@gmail.com>
2015-08-30 16:53:43 -0700

    54

--- 54_Spiral_Matrix.py
+++ 54_Spiral_Matrix.py
@@ -1,24 +1,14 @@
-# 2015-05-19  Runtime: 44 ms
-
+# 2015-08-30  Runtime: 44 ms
 class Solution:
     # @param {integer[][]} matrix
     # @return {integer[]}
     def spiralOrder(self, matrix):
         if not matrix: return []
-        rows, cols = len(matrix), len(matrix[0])
-        direct = ((0, 1), (1, 0), (0, -1), (-1, 0)) # go right, go down, go left, go up
-        currRow, currCol, currDirect, result = 0, -1, 0, [] # at first we go right
+        i, j, di, dj, M, N, res = 0, 0, 0, 1, len(matrix), len(matrix[0]), []
         while True:
-            for i in xrange(cols):
-                currRow, currCol = currRow + direct[currDirect][0], currCol + direct[currDirect][1]
-                result.append(matrix[currRow][currCol])
-            rows -= 1
-            if rows == 0: break
-            currDirect = (currDirect + 1) % 4 # change direction
-            for i in xrange(rows):
-                currRow, currCol = currRow + direct[currDirect][0], currCol + direct[currDirect][1]
-                result.append(matrix[currRow][currCol])
-            cols -= 1
-            if cols == 0: break
-            currDirect = (currDirect + 1) % 4 # change direction
-        return result
+            res.append(matrix[i][j])
+            if len(res) == M * N: return res
+            matrix[i][j] = None
+            if matrix[(i + di) % M][(j + dj) % N] is None:
+                di, dj = dj, -di # turn right
+            i, j = i + di, j + dj
