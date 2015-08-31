e1df674aef22751445b3c4777baa040f3764303e
Chao <chaoren.hust@gmail.com>
2015-08-30 18:54:43 -0700

    60

--- 60_Permutation_Sequence.py
+++ 60_Permutation_Sequence.py
@@ -1,14 +1,14 @@
-# 2015-04-13  Runtime: 50 ms  
-
-class Solution:
-    # @return a string
+# 2015-08-30  Runtime: 44 ms  
+class Solution(object):
     def getPermutation(self, n, k):
-        k -= 1
-        result = ''
-        numberPool = [str(i) for i in xrange(1, n+1)]
-        for i in reversed(xrange(n)): # from n-1 to 0
-            fac = math.factorial(i)
-            result += numberPool[k / fac]
-            del numberPool[k / fac]
-            k %= fac
-        return result
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        k, res, fac, char = k - 1, '', math.factorial(n - 1), ['1', '2', '3', '4', '5', '6', '7', '8', '9'][:n]
+        for i in xrange(n - 1, -1, -1):
+            idx, k = divmod(k, fac)
+            if i: fac /= i
+            res += char.pop(idx)
+        return res
