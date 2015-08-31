dd2e76571a3976011371fd7a18b61778ca8a40e4
Chao <chaoren.hust@gmail.com>
2015-08-31 08:43:30 -0700

    62

--- 62_Unique_Paths.py
+++ 62_Unique_Paths.py
@@ -1,9 +1,14 @@
-# 2015-05-23  Runtime: 40 ms
-class Solution:
-    # @param {integer} m
-    # @param {integer} n
-    # @return {integer}
+# 2015-08-31  Runtime: 56 ms
+class Solution(object):
     def uniquePaths(self, m, n):
-        # in total the robot will move m+n-2 steps, m-1 right, n-1 down
-        # so the answer is C(m+n-2, m-1) = (m+n-2)! / ((m-1)!(n-1)!)
-        return math.factorial(m + n - 2) / math.factorial(m - 1) / math.factorial(n - 1)
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        # C(m-1+n-1)(m-1)
+        if m > n: m, n = n, m
+        res = 1
+        for i in xrange(1, m):
+            res = res * (m + n - 1 - i) / i
+        return res
