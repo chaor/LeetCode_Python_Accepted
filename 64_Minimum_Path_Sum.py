db58f099a06cfe477f7d72be07ce3bd682e6b536
Chao <chaoren.hust@gmail.com>
2015-08-31 21:11:34 -0700

    64

--- 64_Minimum_Path_Sum.py
+++ 64_Minimum_Path_Sum.py
@@ -1,16 +1,14 @@
-# 2015-06-18  Runtime: 76 ms
-class Solution:
-    # @param {integer[][]} grid
-    # @return {integer}
+# 2015-08-31  Runtime: 76 ms
+class Solution(object):
     def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
         M, N = len(grid), len(grid[0])
-        # dp[x][y] is the min sum from grid[0][0] to grid[x][y]
-        dp = [[0 for j in xrange(N)] for i in xrange(M)]
-        dp[0][0] = grid[0][0]
-        for i in xrange(1, M): dp[i][0] = dp[i - 1][0] + grid[i][0]
-        for j in xrange(1, N): dp[0][j] = dp[0][j - 1] + grid[0][j]
-        
-        for i in xrange(1, M):
-            for j in xrange(1, N):
-                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
-        return dp[M - 1][N - 1]
+        Sum = [[10 ** 10] * (N + 1) for i in xrange(M + 1)] # initialize to infinity
+        Sum[M - 1][N] = 0
+        for i in reversed(xrange(M)):
+            for j in reversed(xrange(N)):
+                Sum[i][j] = grid[i][j] + min(Sum[i + 1][j], Sum[i][j + 1])
+        return Sum[0][0]
