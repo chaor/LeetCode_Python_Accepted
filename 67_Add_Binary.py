34c6d75f35ff0ed2e5427c12c8715620e29101b9
Chao <chaoren.hust@gmail.com>
2015-09-01 20:44:13 -0700

    67

--- 67_Add_Binary.py
+++ 67_Add_Binary.py
@@ -1,21 +1,20 @@
-# 2015-06-18  Runtime: 64 ms
-class Solution:
-    # @param {string} a
-    # @param {string} b
-    # @return {string}
+# 2015-06-18  Runtime: 60 ms
+class Solution(object):
     def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
         char2int = {'0': 0, '1': 1}
-        carry, i, j, res = 0, len(a) - 1, len(b) - 1, []
-        while i >= 0 or j >= 0 or carry == 1:
+        i, j, carry, res = len(a) - 1, len(b) - 1, 0, []
+        while i >= 0 or j >= 0 or carry:
             if i >= 0:
                 carry += char2int[a[i]]
                 i -= 1
             if j >= 0:
                 carry += char2int[b[j]]
                 j -= 1
-            if carry % 2 == 0:
-                res.append('0')
-            else:
-                res.append('1')
+            res.append(chr(carry % 2 + 48))
             carry /= 2
         return ''.join(res[::-1])
