2d0fa740b872efbdac768634b982bff7ac843364
Chao <chaoren.hust@gmail.com>
2015-09-07 17:23:12 -0700

    50

--- 50_Pow_x_n.py
+++ 50_Pow_x_n.py
@@ -1,12 +1,15 @@
-# 2015-06-16  Runtime: 60 ms
+# 2015-09-07  Runtime: 60 ms
 class Solution:
     # @param {float} x
     # @param {integer} n
     # @return {float}
     def myPow(self, x, n):
-        if n < 0: return 1.0 / self.myPow(x, -n)
-        if n == 0: return 1.0
-        if n == 1: return x
-        if n % 2 == 0:
-            return self.myPow(x, n / 2) ** 2
-        return self.myPow(x, n / 2) ** 2 * x
+        # thanks to https://leetcode.com/discuss/9459/o-logn-solution-in-java
+        if n < 0:
+            x, n = 1.0 / x, -n
+        res, f = 1, x
+        while n > 0:
+            if n % 2: res *= f
+            f = f * f
+            n >>= 1
+        return res
