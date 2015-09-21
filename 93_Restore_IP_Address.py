8bfe1b63d3812cefb63c07e41eed03ab5b6eb263
Chao <chaoren.hust@gmail.com>
2015-09-20 18:20:37 -0700

    93

--- 93_Restore_IP_Address.py
+++ 93_Restore_IP_Address.py
@@ -1,20 +1,20 @@
-# 2015-05-26  Runtime: 56 ms
-
-class Solution:
-    # @param {string} s
-    # @return {string[]}
+# 2015-09-20  Runtime: 56 ms
+class Solution(object):
     def restoreIpAddresses(self, s):
-        self.answer, self.oneAnswer = [], []
-        self.dfs(s, 0)
-        return self.answer
-        
-    def dfs(self, ip, n):
-        if n == 4:
-            if ip == '': self.answer.append('.'.join(self.oneAnswer))
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        self.result = []
+        self.getIP(s, '', 0)
+        return self.result
+
+    def getIP(self, s, IP, k):
+        if k == 4:
+            if not s: self.result.append(IP[:-1])
             return
         for i in xrange(1, 4):
-            if len(ip) >= i: 
-                if 0 < int(ip[:i]) <= 255 and ip[:i][0] != '0' or ip[:i] == '0': # avoid '010', but we need to keep '0'
-                    self.oneAnswer.append(ip[:i])
-                    self.dfs(ip[i:], n + 1)
-                    self.oneAnswer.pop()
+            if len(s) >= i:
+                first_i = s[:i]
+                if first_i[0] != '0' and 1 <= int(first_i) <= 255 or first_i == '0':
+                    self.getIP(s[i:], IP + first_i + '.', k + 1)
