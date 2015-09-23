a181ddf3525d8971f268a45cd84292df8c3f34f7
Chao <chaoren.hust@gmail.com>
2015-09-23 10:19:53 -0700

    95

--- 95_Unique_Binary_Search_Trees_II.py
+++ 95_Unique_Binary_Search_Trees_II.py
@@ -1,26 +1,26 @@
-# 2015-06-24  Runtime: 104 ms
+# 2015-09-23  Runtime: 92 ms
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    # @param {integer} n
-    # @return {TreeNode[]}
+class Solution(object):
     def generateTrees(self, n):
-        return self.solve([i for i in xrange(1, n + 1)])
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        return self.gen(range(1, n + 1))
         
-    def solve(self, L):
+    def gen(self, L):
         if not L: return [None]
         if len(L) == 1: return [TreeNode(L[0])]
         res = []
         for i in xrange(len(L)):
-            leftList, rightList = self.solve(L[:i]), self.solve(L[i + 1:])
-            for left in leftList:
-                for right in rightList:
-                    root = TreeNode(L[i])
-                    root.left, root.right = left, right
-                    res.append(root)
+            for leftChild in self.gen(L[:i]):
+                for rightChild in self.gen(L[i + 1:]):
+                    res.append(TreeNode(L[i]))
+                    res[-1].left, res[-1].right = leftChild, rightChild
         return res
