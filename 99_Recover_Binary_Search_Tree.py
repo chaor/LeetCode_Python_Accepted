8d4a6cdf4f808fd4b492fa0ef557d666b48afd9c
Chao <chaoren.hust@gmail.com>
2015-09-23 23:47:25 -0700

    99

--- 99_Recover_Binary_Search_Tree.py
+++ 99_Recover_Binary_Search_Tree.py
@@ -1,25 +1,31 @@
-# 2015-06-25  Runtime: 172 ms
+# 2015-09-23  Runtime: 140 ms
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    # @param {TreeNode} root
-    # @return {void} Do not return anything, modify root in-place instead.
+class Solution(object):
     def recoverTree(self, root):
-        # thanks to https://leetcode.com/discuss/13034/no-fancy-algorithm-just-simple-and-powerful-order-traversal
-        self.prev, self.first, self.second = TreeNode(-10 ** 10), None, None
-        self.inorder(root)
-        self.first.val, self.second.val = self.second.val, self.first.val
-        
-    def inorder(self, node):
-        if node.left: self.inorder(node.left)
-        if not self.first and self.prev.val >= node.val:
-            self.first = self.prev
-        if self.first and self.prev.val >= node.val:
-            self.second = node
-        self.prev = node
-        if node.right: self.inorder(node.right)
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        curr, pre, preFirst, first, second, stack = root, TreeNode(-10 ** 10), None, None, None, []
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            if curr.val < pre.val:
+                if not first:
+                    first, preFirst = curr, pre
+                else:
+                    second = curr
+            pre = curr    
+            curr = curr.right
+        if second:
+            preFirst.val, second.val = second.val, preFirst.val
+        else:
+            preFirst.val, first.val = first.val, preFirst.val
