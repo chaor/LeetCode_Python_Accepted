bec247503a0e4d50984a9afa6d422ee022d34774
Chao <chaoren.hust@gmail.com>
2015-09-23 22:51:23 -0700

    98

--- 98_Validate_Binary_Search_Tree.py
+++ 98_Validate_Binary_Search_Tree.py
@@ -1,22 +1,23 @@
-# 2015-05-17  Runtime: 76 ms
+# 2015-09-23  Runtime: 80 ms
 
-# Definition for a  binary tree node
-# class TreeNode:
+# Definition for a binary tree node.
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    # @param root, a tree node
-    # @return a boolean
+class Solution(object):
     def isValidBST(self, root):
-        return self.check(root, None, None) # first None means infinitesimal, second None means infinite large
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.check(root, -10 ** 10, 10 ** 10)
         
-    def check(self, node, low, high):
-        if not node: return True
-        return (low == None or low < node.val) and (high == None or node.val < high) \
-            and self.check(node.left, low, node.val) and self.check(node.right, node.val, high)
+    def check(self, root, L, R):
+        if not root: return True
+        return self.check(root.left, L, root.val) and L < root.val < R and self.check(root.right, root.val, R) 
 
 ################Another Solution###################
 class Solution:
