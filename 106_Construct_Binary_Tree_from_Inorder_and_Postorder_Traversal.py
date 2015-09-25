8042baa385a0cbf9026a056ae5565d858e13aa7d
Chao <chaoren.hust@gmail.com>
2015-09-24 23:50:35 -0700

    106

--- 106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
+++ 106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
@@ -1,25 +1,24 @@
-# 2015-03-20  Runtime: 116 ms
-
+# 2015-09-24  Runtime: 112 ms
+# Definition for a  binary tree node
 # class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
- 
+
 class Solution:
     # @param inorder, a list of integers
     # @param postorder, a list of integers
     # @return a tree node
     def buildTree(self, inorder, postorder):
-        if not inorder: return None # inorder is empty
         self.inorder, self.postorder = inorder, postorder
-        return self.dfs(0, 0, len(inorder))
-    
-    def dfs(self, inLeft, postLeft, Len):
-        if Len <= 0:
-            return None
-        root = TreeNode(self.postorder[postLeft + Len - 1])
-        rootPos = self.inorder.index(self.postorder[postLeft + Len - 1])
-        root.left = self.dfs(inLeft, postLeft, rootPos - inLeft)
-        root.right = self.dfs(rootPos + 1, postLeft + rootPos - inLeft, Len - 1 - (rootPos - inLeft))
-        return root
+        return self.build(0, len(self.inorder) - 1, 0, len(self.postorder) - 1)
+        
+    def build(self, L1, R1, L2, R2):
+        if L1 > R1: return None
+        node = TreeNode(self.postorder[R2])
+        if L1 == R1: return node
+        ind = self.inorder.index(self.postorder[R2])
+        node.left = self.build(L1, ind - 1, L2, L2 + ind - 1 - L1)
+        node.right = self.build(ind + 1, R1, L2 + ind - L1, R2 - 1)
+        return node
