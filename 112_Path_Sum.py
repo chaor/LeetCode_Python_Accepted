4f2560b3424bde9497f27cc4045b3f3a42338068
Chao <chaoren.hust@gmail.com>
2015-09-26 12:41:21 -0700

    112

--- 112_Path_Sum.py
+++ 112_Path_Sum.py
@@ -1,24 +1,24 @@
-# 20150323  Runtime: 138 ms
-
-# Definition for a  binary tree node
-# class TreeNode:
+# 2015-09-26  Runtime: 64 ms
+# Definition for a binary tree node.
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    # @param root, a tree node
-    # @param sum, an integer
-    # @return a boolean
+class Solution(object):
     def hasPathSum(self, root, sum):
-        if not root:
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root: 
             return False
-        if not (root.left or root.right):
-            return root.val == sum
-        res = False
-        if root.left:
-            res = self.hasPathSum(root.left, sum - root.val)
-        if root.right:
-            res = res or self.hasPathSum(root.right, sum - root.val)
-        return res
+        if not root.left and not root.right:
+            return sum == root.val
+        if root.left and self.hasPathSum(root.left, sum - root.val):
+            return True
+        if root.right and self.hasPathSum(root.right, sum - root.val):
+            return True
+        return False
