ca380295ffd49343cb7694a537b66562137aef32
Chao <chaoren.hust@gmail.com>
2015-09-29 09:28:45 -0700

    188

--- 188_Best_Time_to_Buy_and_Sell_Stock_IV.py
+++ 188_Best_Time_to_Buy_and_Sell_Stock_IV.py
@@ -1,20 +1,25 @@
-# 2015-04-04  Runtime: 175 ms
-
-class Solution:
-    # @return an integer as the maximum profit 
+# 2015-09-29  Runtime: 120 ms
+class Solution(object):
     def maxProfit(self, k, prices):
-        if k <= len(prices) / 2:
-            # dp[N] means the max profit after Nth buy or sell, N=1,3,5,7... buy, N=0,2,4,6,8... sell
-            # 状态转移方程 dp[j] = max(dp[j], dp[j - 1] + prices[i] * [1, -1][j % 2])
-            dp = [None for i in xrange(2 * k + 1)]
-            dp[0] = 0
-            for i in xrange(len(prices)):
-                for j in xrange(1, min(2 * k + 1, i + 2)):
-                    dp[j] = max(dp[j], dp[j - 1] + prices[i] * [1, -1][j % 2])
-            return max(dp)
-        else: # k > len(prices) / 2, we can do unlimited transactions, it becomes Best Time to Buy and Sell Stock II
-            sum = 0
-            for i in xrange(1, len(prices)):
+        """
+        :type k: int
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices or not k: return 0
+        n = len(prices)
+        if k <= n / 2:
+            kthBuy, kthSell = [-sys.maxint] * k, [0] * k
+            for price in prices:
+                for i in xrange(k - 1, 0, -1):
+                    kthSell[i] = max(kthSell[i], kthBuy[i] + price)
+                    kthBuy[i] = max(kthBuy[i], kthSell[i - 1] - price)
+                kthSell[0] = max(kthSell[0], kthBuy[0] + price)
+                kthBuy[0] = max(kthBuy[0], -price)
+            return kthSell[k - 1]
+        else: # we can do the transaction unlimited times
+            res = 0
+            for i in xrange(1, n):
                 if prices[i] > prices[i - 1]:
-                    sum += prices[i] - prices[i - 1]
-            return sum
+                    res += prices[i] - prices[i - 1]
+            return res
