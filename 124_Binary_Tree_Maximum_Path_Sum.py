d0d56b09881f51d79b4bf8e423087b12182e18d1
Chao <chaoren.hust@gmail.com>
2015-09-29 09:58:19 -0700

    124

--- 124_Binary_Tree_Maximum_Path_Sum.py
+++ 124_Binary_Tree_Maximum_Path_Sum.py
@@ -1,23 +1,27 @@
-# 2015-05-17  Runtime: 184 ms
-
+# 2015-09-29  Runtime: 156 ms
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    # @param {TreeNode} root
-    # @return {integer}
+class Solution(object):
     def maxPathSum(self, root):
-        self.maxSum = -10 ** 10 # infinitesimal
-        self.findMax(root)
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        self.maxSum = -sys.maxint
+        self.calc(root)
         return self.maxSum
         
-    def findMax(self, node):
-        if not node: return 0
-        LMax, RMax = self.findMax(node.left), self.findMax(node.right)
-        self.maxSum = max(self.maxSum, LMax + node.val + RMax)
-        ret = node.val + max(LMax, RMax)
-        return ret if ret > 0 else 0
+    def calc(self, node):
+        L, R = 0, 0
+        if node.left:
+            L = self.calc(node.left)
+        if node.right:
+            R = self.calc(node.right)
+        self.maxSum = max(self.maxSum, L + node.val + R)
+        return max(0, node.val, node.val + L, node.val + R)
