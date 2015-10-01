5310f90713a22c1dd17c8c852251b53b791b8937
Chao <chaoren.hust@gmail.com>
2015-09-30 23:33:35 -0700

    129

--- 129_Sum_Root_Leaf_Numbers.py
+++ 129_Sum_Root_Leaf_Numbers.py
@@ -1,28 +1,28 @@
-# 2014-03-17  Runtime: 61 ms
-
-# Definition for a  binary tree node
-# class TreeNode:
+# 2015-09-30  Runtime: 48 ms
+# Definition for a binary tree node.
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    # @param root, a tree node
-    # @return an integer
+class Solution(object):
     def sumNumbers(self, root):
-        if not root:
-            return 0
-        self.Sum = 0
-        self.dfs(root, '')
-        return self.Sum
-    
-    def dfs(self, node, s):
-        if not (node.left or node.right):
-            self.Sum += int(s + str(node.val))
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        #self.d = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9}
+        self.res = 0
+        self.calc(root, 0)
+        return self.res
+        
+    def calc(self, node, x):
+        if not node.left and not node.right:
+            self.res += 10 * x + node.val
             return
         if node.left:
-            self.dfs(node.left, s + str(node.val))
+            self.calc(node.left, 10 * x + node.val)
         if node.right:
-            self.dfs(node.right, s + str(node.val))
-        return
+            self.calc(node.right, 10 * x + node.val)
