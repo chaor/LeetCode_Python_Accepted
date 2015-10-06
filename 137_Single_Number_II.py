bf3ba83449d109bdfb314e0f713b2ffba6e2cee1
Chao <chaoren.hust@gmail.com>
2015-10-05 23:21:57 -0700

    137

--- 137_Single_Number_II.py
+++ 137_Single_Number_II.py
@@ -1,16 +1,13 @@
-# 2015-05-19  Runtime: 52 ms
-
-class Solution:
-    # @param {integer[]} nums
-    # @return {integer}
+# 2015-10-05   Runtime: 44 ms
+# thanks to https://leetcode.com/discuss/6632/challenge-me-thx, see woshidaishu's explanation
+class Solution(object):
     def singleNumber(self, nums):
-        # ones, twos, threes represent the ith bit had appeared once, twice, three times
-        ones, twos, threes = 0, 0, 0
-        for i in xrange(len(nums)):
-            twos |= ones & nums[i]
-            ones ^= nums[i]
-            threes = ones & twos
-            # When the ith bit had appeared for the third time, clear the ith bit of both ones and twos to 0.
-            ones &= ~threes
-            twos &= ~threes
-        return ones
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a, b = 0, 0
+        for x in nums:
+            a = (a ^ x) & b
+            b = (b ^ x) | a
+        return b
