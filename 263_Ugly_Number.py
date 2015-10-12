d14cf0f5a7fd7807d28fea1f60382dbbd1204d09
Chao <chaoren.hust@gmail.com>
2015-10-12 09:20:39 -0700

    263

---
+++ 263_Ugly_Number.py
@@ -0,0 +1,12 @@
+# 2015-10-12  Runtime: 60 ms
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 0: return False
+        while num % 2 == 0: num /= 2
+        while num % 3 == 0: num /= 3
+        while num % 5 == 0: num /= 5
+        return num == 1
