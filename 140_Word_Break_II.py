c0d84f54dd325d84a307065643ce17c1a8938e58
Chao <chaoren.hust@gmail.com>
2015-10-13 22:30:59 -0700

    140

--- 140_Word_Break_II.py
+++ 140_Word_Break_II.py
@@ -1,28 +1,27 @@
-# 2014-03-15, Runtime: 118 ms
+# 2014-10-13  Runtime: 148 ms
 
-class Solution:
-    # @param s, a string
-    # @param dict, a set of string
-    # @return a list of strings
-    def wordBreak(self, s, dict):
-        self.s = s
-        self.dict = dict
-        # 使用matchable剪枝，matchable[j] == True指s[i:]可以被成功分成若干单词
-        # use matchable to pruning, matchable[j] == True means s[i:] can be successfully seperated into several words
-        self.matchable = [False for i in xrange(len(s) + 1)]
-        self.matchable[len(s)] = True
-        for i in reversed(xrange(len(s))):
-            for word in dict:
-                if i + len(word) <= len(s) and word == s[i:i+len(word)]:
-                    self.matchable[i] = self.matchable[i] or self.matchable[i+len(word)]
-        self.results = []
-        self.dfs(0, [])
-        return self.results
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: Set[str]
+        :rtype: List[str]
+        """
+        self.s, self.wordDict, self.lenS = s, wordDict, len(s)
+        # 使用matchable剪枝，matchable[j] == True指s[i:]可以被成功分成若干单词，
+        self.matchable = [False for i in xrange(self.lenS)] + [True]
+        for i in reversed(xrange(self.lenS)):
+            for word in wordDict:
+                if i + len(word) <= self.lenS and word == s[i: i + len(word)]:
+                    self.matchable[i] = self.matchable[i] or self.matchable[i + len(word)]
+        self.result = []
+        self.dfs(0, '')
+        return self.result
         
-    def dfs(self, k, oneResult):
+    def dfs(self, k, oneAnswer):
         if k >= len(self.s):
-            self.results.append(' '.join(oneResult))
+            self.result.append(oneAnswer[:-1])
             return
-        for word in self.dict:
-            if k + len(word) <= len(self.s) and self.matchable[k + len(word)] and self.s[k:k+len(word)] == word:
-                self.dfs(k + len(word), oneResult + [word])
+        for word in self.wordDict:
+            if k + len(word) <= self.lenS and self.matchable[k + len(word)] and self.s[k: k + len(word)] == word:
+                self.dfs(k + len(word), oneAnswer + word + ' ')
