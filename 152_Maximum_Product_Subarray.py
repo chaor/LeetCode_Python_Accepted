849a6b12437c556e94c2c50d894233f6f2249a8e
Chao <chaoren.hust@gmail.com>
2015-10-15 10:43:54 -0700

    152

--- 152_Maximum_Product_Subarray.py
+++ 152_Maximum_Product_Subarray.py
@@ -1,13 +1,12 @@
-# 2015-05-27  Runtime: 60 ms
-
-class Solution:
-    # @param {integer[]} nums
-    # @return {integer}
+# 2015-10-15  Runtime: 52 ms
+class Solution(object):
     def maxProduct(self, nums):
-        # let fk = maxProduct whose end index is k, gk = minProduct whose end index is k
-        # if current element > 0, fk * current will be next maxProduct, otherwise gk * current will be next maxProduct
-        Max, fk, gk = nums[0], nums[0], nums[0]
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxP, minP, res = nums[0], nums[0], nums[0]
         for i in xrange(1, len(nums)):
-            fk, gk = max(fk * nums[i], nums[i], gk * nums[i]), min(fk * nums[i], nums[i], gk * nums[i])
-            Max = max(Max, fk)
-        return Max
+            maxP, minP = max(nums[i], maxP * nums[i], minP * nums[i]), min(nums[i], maxP * nums[i], minP * nums[i])
+            if maxP > res: res = maxP
+        return res
