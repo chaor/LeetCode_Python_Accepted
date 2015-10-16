125ef8277472738b00ab0c014b10a1c78071c910
Chao <chaoren.hust@gmail.com>
2015-10-15 20:01:28 -0700

    147

--- 147_Insertion_Sort_List.py
+++ 147_Insertion_Sort_List.py
@@ -1,25 +1,23 @@
-# 2014-12-25  Runtime: 928 ms  Merry Chirstmas! 圣诞节快乐!
-
+# 2015-10-15   Runtime: 360 ms
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
-    # @param head, a ListNode
-    # @return a ListNode
+class Solution(object):
     def insertionSortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
         if not head or not head.next: return head
-        # add a dummy node in head, 在链表头部加一个空节点
-        dummy = ListNode(-10**4)
-        dummy.next = head
+        dummyHead = ListNode(0)
+        dummyHead.next = head
         cur = head
         while cur.next:
             if cur.val > cur.next.val:
-                # insert cur.next into proper position, note that head's position may change, but dummy's position is fixed
-                # 把cur.next插入前面合适的位置，注意head所指的节点在链表中的位置会变，而dummy所指的节点在链表中的位置不变
-                pPre, p = dummy, dummy.next
+                pPre, p = dummyHead, dummyHead.next
                 while p.val < cur.next.val:
                     pPre, p = pPre.next, p.next
                 tmp = cur.next
@@ -28,4 +26,4 @@ class Solution:
                 pPre.next = tmp
             else:
                 cur = cur.next
-        return dummy.next
+        return dummyHead.next
