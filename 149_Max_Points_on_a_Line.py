ab9d536eba3c80be4f8d512412fbf2ae77ba158d
Chao <chaoren.hust@gmail.com>
2015-10-20 18:50:34 -0700

    149

--- 149_Max_Points_on_a_Line.py
+++ 149_Max_Points_on_a_Line.py
@@ -1,32 +1,29 @@
-# 2014-12-24  Runtime: 320 ms
-
-# Definition for a point
-# class Point:
+# 2015-10-20  Runtime: 104 ms
+# Definition for a point.
+# class Point(object):
 #     def __init__(self, a=0, b=0):
 #         self.x = a
 #         self.y = b
 
-class Solution:
-    # @param points, a list of Points
-    # @return an integer
+class Solution(object):
     def maxPoints(self, points):
-        pointsNum = len(points)
-        if pointsNum <= 2:
-            return pointsNum
-        # for each point, use dictionary slope to record point number of each slope
-        # 对每个点，用字典slope记录每个斜率上点的个数
-        res = -1
+        """
+        :type points: List[Point]
+        :rtype: int
+        """
+        if len(points) <= 2: return len(points)
+        result = 0
         for p1 in points:
-            slope = {}
-            samePointNum = 0
+            d, d_max, vertical, same = {}, 0, 0, 0
             for p2 in points:
-                if p1 is not p2:
-                    if p1.x == p2.x and p1.y == p2.y: # the same point
-                        samePointNum += 1
-                    elif p1.x != p2.x:
-                        k = 1.0 * (p2.y - p1.y) / (p2.x - p1.x)
-                        slope[k] = 2 if k not in slope else slope[k] + 1
-                    else: # slope doesn't exist or is infinite, 斜率不存在的情况
-                        slope['vertical'] = 2 if 'vertical' not in slope else slope['vertical'] + 1
-            res = max(res, max(slope.values()) + samePointNum if slope else 1 + samePointNum)
-        return res
+                if p2.x == p1.x:
+                    if p2.y == p1.y:
+                        same += 1
+                    else:
+                        vertical += 1
+                else:
+                    k = (p2.y - p1.y) * 1.0 / (p2.x - p1.x)
+                    d[k] = d[k] + 1 if k in d else 1
+                    d_max = max(d_max, d[k])
+            result = max(result, d_max + same, vertical + same)
+        return result
