6901412c42effd323177288439aae1066a771d2f
Chao <chaoren.hust@gmail.com>
2015-11-05 14:38:14 -0800

    173

--- 173_Binary_Search_Tree_Iterator.py
+++ 173_Binary_Search_Tree_Iterator.py
@@ -1,37 +1,39 @@
-2014-01-09  Runtime: 148 ms
+# 20151105  Runtime: 104 ms
+# Non-recursive inorder traversal
 
 # Definition for a  binary tree node
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class BSTIterator:
-    # inorder traversal, O(h) memory means we need to store the most left branch
-    # 中序遍历，O(h)的内存限制暗示要存储最左边的一串节点
-    
-    # @param root, a binary search tree's root node
+class BSTIterator(object):
     def __init__(self, root):
-        self.stack = []
-        self.pushMostLeft(root)
+        """
+        :type root: TreeNode
+        """
+        self.stack, p = [], root
+        while p:
+            self.stack.append(p)
+            p = p.left
 
-    # @return a boolean, whether we have a next smallest number
     def hasNext(self):
-        return True if self.stack else False
+        """
+        :rtype: bool
+        """
+        return self.stack
 
-    # @return an integer, the next smallest number
     def next(self):
+        """
+        :rtype: int
+        """
         node = self.stack.pop()
-        if node.right:
-            self.pushMostLeft(node.right)
+        p = node.right
+        while p:
+            self.stack.append(p)
+            p = p.left
         return node.val
-        
-    # @param current node, push most left nodes to stack
-    def pushMostLeft(self, curNode):
-        while curNode:
-            self.stack.append(curNode)
-            curNode = curNode.left
 
 # Your BSTIterator will be called like this:
 # i, v = BSTIterator(root), []
