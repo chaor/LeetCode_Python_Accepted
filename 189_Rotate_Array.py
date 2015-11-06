c3840492468bf6b62a63a95c715ff53c7487da3e
Chao <chaoren.hust@gmail.com>
2015-11-06 14:11:39 -0800

    189

--- 189_Rotate_Array.py
+++ 189_Rotate_Array.py
@@ -1,9 +1,21 @@
-# 2015-03-01  Runtime: 223 ms
-
-class Solution:
-    # @param nums, a list of integer
-    # @param k, num of steps
-    # @return nothing, please modify the nums list in-place.
+# 2015-11-06  Runtime: 84 ms
+class Solution(object):
     def rotate(self, nums, k):
-        for i in xrange(k):
-            nums.insert(0, nums.pop()) 
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        nums.reverse()
+        # reverse the first half
+        self.reverse(nums, 0, k - 1)
+        # reverse the second half
+        self.reverse(nums, k, n - 1)
+            
+    def reverse(self, nums, L, R):
+        while L < R:
+            nums[L], nums[R] = nums[R], nums[L]
+            L += 1
+            R -= 1
