8d5d8146ec275e04a0661ce689cd1ec7ae62bfdc
Chao <chaoren.hust@gmail.com>
2015-11-08 12:24:58 -0800

    198

--- 198_House_Robber.py
+++ 198_House_Robber.py
@@ -1,14 +1,11 @@
-# 2015-11-06  Runtime: 40 ms
+# 2015-11-08  Runtime: 44 ms
 class Solution(object):
     def rob(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        N = len(nums)
-        if N == 0: return 0
-        if N == 1: return nums[0]
-        dp = [nums[0], max(nums[0], nums[1])] + [0] * (N - 2)
-        for i in range(2, N):
-            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
-        return dp[N - 1]
+        include, exclude = 0, 0
+        for money in nums:
+            include, exclude = money + exclude, max(include, exclude)
+        return max(include, exclude)
