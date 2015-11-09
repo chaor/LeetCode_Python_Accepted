976187bd3d1b7c3c0394877ebc8a6d19a56f7221
Chao <chaoren.hust@gmail.com>
2015-11-08 20:53:33 -0800

    117

--- 117_Populating_Next_Right_Pointers_in_Each_Node_II.py
+++ 117_Populating_Next_Right_Pointers_in_Each_Node_II.py
@@ -1,27 +1,28 @@
-# 2015-06-26  Runtime: 172 ms
+# 2015-11-08  Runtime: 100 ms
 # Definition for binary tree with next pointer.
-# class TreeLinkNode:
+# class TreeLinkNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 #         self.next = None
 
-class Solution:
-    # @param root, a tree link node
-    # @return nothing
+class Solution(object):
     def connect(self, root):
-        curr = root
-        while curr:
-            nextLevelFirstNode, prev = None, None
+        """
+        :type root: TreeLinkNode
+        :rtype: nothing
+        """
+        nextFirst = root
+        while nextFirst:
+            prev, nextFirst, curr = None, None, nextFirst
             while curr:
-                if not nextLevelFirstNode:
-                    nextLevelFirstNode = curr.left if curr.left else curr.right
+                if not nextFirst:
+                    nextFirst = curr.left if curr.left else curr.right
                 if curr.left:
                     if prev: prev.next = curr.left
                     prev = curr.left
                 if curr.right:
                     if prev: prev.next = curr.right
                     prev = curr.right
-                curr = curr.next
-            curr = nextLevelFirstNode # go to next level    
+                curr = curr.next  
