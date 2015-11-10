6e89e8141c6b2c5323701e70729844ea6b0fab23
Chao <chaoren.hust@gmail.com>
2015-11-09 19:40:50 -0800

    216

--- 216_Combination_Sum_III.py
+++ 216_Combination_Sum_III.py
@@ -1,16 +1,20 @@
-# 2015-06-03  Runtime: 60 ms
-# thanks to https://leetcode.com/discuss/17932/dp-solution-in-python
-class Solution:
-    # @param {integer} k
-    # @param {integer} n
-    # @return {integer[][]}
+# 2015-11-09  Runtime: 44 ms
+class Solution(object):
     def combinationSum3(self, k, n):
-        # dp[x] is a set of tuples, e.g. when k=3, dp[7] = set([(1,2,4)])
-        dp = [set() for i in xrange(n + 1)]
-        for number in xrange(1, 10):
-            if number > n: break
-            for i in xrange(n - number, 0, -1):
-                # set operation: s | t is equivalent to s.union(t)
-                dp[number + i] |= {Tuple + (number,) for Tuple in dp[i] if len(Tuple) < k} 
-            dp[number].add((number, ))
-        return [list(Tuple) for Tuple in dp[n] if len(Tuple) == k]
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        def dfs(L, k, n, oneAnswer):
+            if n < 0 or k < 0:
+                return
+            if not k and not n:
+                result.append(oneAnswer)
+                return
+            for i in range(len(L)):
+                dfs(L[i + 1:], k - 1, n - L[i], oneAnswer + [L[i]])
+                    
+        result = []
+        dfs([1, 2, 3, 4, 5, 6, 7, 8, 9], k, n, [])
+        return result
