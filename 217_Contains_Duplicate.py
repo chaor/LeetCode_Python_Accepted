87f3733e826ec5bf0a4955ee44ec791eb019b28c
Chao <chaoren.hust@gmail.com>
2015-11-09 19:50:47 -0800

    217

--- 217_Contains_Duplicate.py
+++ 217_Contains_Duplicate.py
@@ -1,10 +1,8 @@
-# 2015-06-23  Runtime: 68 ms
-class Solution:
-    # @param {integer[]} nums
-    # @return {boolean}
+# 2015-11-09  Runtime: 52 ms
+class Solution(object):
     def containsDuplicate(self, nums):
-        count = collections.defaultdict(int)
-        for i in nums:
-            if count[i] >= 1: return True
-            count[i] += 1
-        return False
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(set(nums)) < len(nums)
