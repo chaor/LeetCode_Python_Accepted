9450c4689098bdc4227b450ef5dd267ef1eee081
Chao <chaoren.hust@gmail.com>
2015-11-09 23:38:33 -0800

    3

--- 3_Longest_Substring_Without_Repeating_Characters.py
+++ 3_Longest_Substring_Without_Repeating_Characters.py
@@ -1,14 +1,16 @@
-# 2015-05-10  Runtime: 154 ms
-class Solution:
-    # @param {string} s
-    # @return {integer}
+# 2015-11-09  Runtime: 124 ms
+class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        # key: ASCII 0 - 128, value:  last time appearance index of this char
-        lastTimeIndex = {chr(i):-1 for i in xrange(128)}
-        noRepeatingSinceThis, substringMaxLen = 0, 0
-        for i in xrange(len(s)):
-            if lastTimeIndex[s[i]] >= noRepeatingSinceThis:
-                noRepeatingSinceThis = lastTimeIndex[s[i]] + 1
-            lastTimeIndex[s[i]] = i
-            substringMaxLen = max(i - noRepeatingSinceThis + 1, substringMaxLen)
-        return substringMaxLen
+        """
+        :type s: str
+        :rtype: int
+        """
+        lastAppear, L, maxLen = [-1] * 128, 0, 0
+        for R in range(len(s)):
+            # lastAppear[ord(s[R])] >= L is important bacause we only consider
+            # letters inside the sliding window
+            if lastAppear[ord(s[R])] != -1 and lastAppear[ord(s[R])] >= L:
+                L = lastAppear[ord(s[R])] + 1
+            lastAppear[ord(s[R])] = R
+            maxLen = max(maxLen, R - L + 1)
+        return maxLen
