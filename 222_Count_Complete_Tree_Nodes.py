f19b41170374984280414c58b2b8d98912def32c
Chao <chaoren.hust@gmail.com>
2015-11-10 11:36:16 -0800

    222

--- 222_Count_Complete_Tree_Nodes.py
+++ 222_Count_Complete_Tree_Nodes.py
@@ -1,23 +1,25 @@
-# 2015-07-09  Runtime: 440 ms
+# 2015-11-10  Runtime: 164 ms
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    # @param {TreeNode} root
-    # @return {integer}
+class Solution(object):
     def countNodes(self, root):
-        # thanks to https://leetcode.com/discuss/38944/time-limit-for-the-python-solution
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
         if not root: return 0
-        h1, h2 = 0, 0
-        node = root
-        while node:
-            h1, node = h1 + 1, node.left
-        node = root
-        while node:
-            h2, node = h2 + 1, node.right
-        if h1 == h2: return 2 ** h1 - 1
-        return self.countNodes(root.left) + 1 + self.countNodes(root.right)
+        if not root.left and not root.right: return 1
+        p1, p2, L, R = root.left, root.right, 0, 0
+        while p1:
+            p1, L = p1.left, L + 1
+        while p2:
+            p2, R = p2.left, R + 1
+        if L == R:
+            return 1 + 2 ** L - 1 + self.countNodes(root.right)
+        else:
+            return 1 + 2 ** R - 1 + self.countNodes(root.left)
