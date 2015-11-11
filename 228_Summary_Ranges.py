18c356f96b9eb9ec6963e99efdd5ee84b719a850
Chao <chaoren.hust@gmail.com>
2015-11-10 22:05:44 -0800

    228

--- 228_Summary_Ranges.py
+++ 228_Summary_Ranges.py
@@ -1,16 +1,25 @@
-# 2015-06-25  Runtime: 48 ms
-class Solution:
-    # @param {integer[]} nums
-    # @return {string[]}
+# 2015-11-10  Runtime: 36 ms
+class Solution(object):
     def summaryRanges(self, nums):
-        if len(nums) <= 1: return [str(i) for i in nums]
-        nums.append(10**10) # infinity
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        if not nums: return []
         start, res = nums[0], []
-        for i in xrange(1, len(nums)):
-            if nums[i] != nums[i-1] + 1:
-                if nums[i-1] == start:
-                    res.append(str(start))
-                else:
-                    res.append(str(start) + '->' + str(nums[i-1]))
-                start = nums[i]
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1] + 1:
+                continue
+            if start != nums[i - 1]:
+                res.append(str(start) + '->' + str(nums[i - 1]))
+            else:
+                res.append(str(start))
+            start = nums[i]
+        # sometimes we can't modify nums
+        # if we can modify nums, we can nums.append(sys.maxint)
+        # and then we can just return res
+        if start != nums[-1]:
+            res.append(str(start) + '->' + str(nums[-1]))
+        else:
+            res.append(str(start))
         return res
