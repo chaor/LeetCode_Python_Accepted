4f8ac9ef52d625a1546a45dc5795d182e9202fe8
Chao <chaoren.hust@gmail.com>
2015-11-10 17:10:36 -0800

    223

--- 223_Rectangle_Area.py
+++ 223_Rectangle_Area.py
@@ -1,13 +1,17 @@
-# 2015-06-07  Runtime: 57 ms
-class Solution:
-    # @param {integer} A
-    # @param {integer} B
-    # @param {integer} C
-    # @param {integer} D
-    # @param {integer} E
-    # @param {integer} F
-    # @param {integer} G
-    # @param {integer} H
-    # @return {integer}
+# 2015-11-10  Runtime: 128 ms
+class Solution(object):
     def computeArea(self, A, B, C, D, E, F, G, H):
-        return (D - B) * (C - A) + (H - F) * (G - E) - (min(C, G) - max(A, E)) * (min(D, H) - max(B, F))
+        """
+        :type A: int
+        :type B: int
+        :type C: int
+        :type D: int
+        :type E: int
+        :type F: int
+        :type G: int
+        :type H: int
+        :rtype: int
+        """
+        twoArea = (D - B) * (C - A) + (H - F) * (G - E)
+        length, width = min(C, G) - max(A, E), min(D, H) - max(B, F)
+        return twoArea - length * width if length > 0 and width > 0 else twoArea
