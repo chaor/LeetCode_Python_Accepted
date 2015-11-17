ee2f3e9ddb639ab99c3b46cd7f73210e62a6b09c
Chao <chaoren.hust@gmail.com>
2015-11-16 23:25:01 -0800

    239

--- 239_Sliding_Window_Maximum.py
+++ 239_Sliding_Window_Maximum.py
@@ -1,17 +1,18 @@
-# 2015-07-18  Runtime: 196 ms
-class Solution:
-    # @param {integer[]} nums
-    # @param {integer} k
-    # @return {integer[]}
+# 2015-11-16  Runtime: 220 ms
+class Solution(object):
     def maxSlidingWindow(self, nums, k):
-        if not nums or k == 0: return []
-        queue = collections.deque(nums[:k])
-        queue_max = max(queue)
-        res = [queue_max]
-        for i in xrange(k, len(nums)):
-            poped = queue.popleft()
-            queue.append(nums[i])
-            if nums[i] > queue_max: queue_max = nums[i]
-            elif queue_max == poped: queue_max = max(queue)
-            res.append(queue_max)
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        q, res = collections.deque(), []
+        for i in range(len(nums)):
+            while q and nums[q[-1]] <= nums[i]:
+                q.pop()
+            q.append(i)
+            if i >= k - 1:
+                while q[0] <= i - k:
+                    q.popleft()
+                res.append(nums[q[0]])
         return res
