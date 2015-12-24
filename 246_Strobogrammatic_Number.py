743199b781410624817cb3e9a2ff5b39aeea44d5
Chao <chaoren.hust@gmail.com>
2015-12-24 11:37:29 -0800

    246

--- 246_Strobogrammatic_Number.py
+++ 246_Strobogrammatic_Number.py
@@ -1,12 +1,10 @@
-# 2015-08-18  Runtime: 44 ms
-
+# 2015-12-24  47 tests, 36 ms
 class Solution:
     # @param {string} num
     # @return {boolean}
     def isStrobogrammatic(self, num):
         L, R = 0, len(num) - 1
-        while L < R:
+        while L <= R:
             if (num[L], num[R]) not in (('0', '0'), ('1', '1'), ('8', '8'), ('6', '9'), ('9', '6')): return False
             L, R = L + 1, R - 1
-        if L == R: return num[L] in '018'
         return True
