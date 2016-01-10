8335b2ae45d128e9328b96fc799505736d8b318d
Chao <chaoren.hust@gmail.com>
2016-01-09 22:58:07 -0800

    307

---
+++ 307_Range_Sum_Query-Mutable.py
@@ -0,0 +1,42 @@
+# 2016-01-09  10 tests, 148 ms
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.n = len(nums)
+        self.a, self.c = nums, [0] * (self.n + 1)
+        for i in range(self.n):
+            k = i + 1
+            while k <= self.n:
+                self.c[k] += nums[i]
+                k += (k & -k)
+
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: int
+        """
+        diff, self.a[i] = val - self.a[i], val
+        i += 1
+        while i <= self.n:
+            self.c[i] += diff
+            i += (i & -i)
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        res, j = 0, j + 1
+        while j:
+            res += self.c[j]
+            j -= (j & -j)
+        while i:
+            res -= self.c[i]
+            i -= (i & -i)
+        return res
