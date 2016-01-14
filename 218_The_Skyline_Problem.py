57a8b604db3449930fbf7f10b6006f08ba29636c
Chao <chaoren.hust@gmail.com>
2016-01-13 23:16:33 -0800

    218

--- 218_The_Skyline_Problem.py
+++ 218_The_Skyline_Problem.py
@@ -1,24 +1,17 @@
-# 2015-06-30  Runtime: 144 ms
-from heapq import *
-
-class Solution:
-    # @param {integer[][]} buildings
-    # @return {integer[][]}
+# 2016-01-13  33 tests, 104 ms
+class Solution(object):
     def getSkyline(self, buildings):
-        LRH = [{'L':i[0], 'R':i[1], 'H':i[2]} for i in buildings]
-        skyline, i, n = [], 0, len(LRH)
-        maxHeap = [] # heappush, heappop is for min heap, so we store negative number
-        while i < n or maxHeap:
-            if not maxHeap or i < n and LRH[i]['L'] <= -maxHeap[0]['R']:
-                x = LRH[i]['L']
-                while i < n and LRH[i]['L'] == x:
-                    heappush(maxHeap, {'H':-LRH[i]['H'], 'R':-LRH[i]['R']})
-                    i += 1
-            else:
-                x = -maxHeap[0]['R']
-                while maxHeap and -maxHeap[0]['R'] <= x:
-                    heappop(maxHeap)
-            height = -maxHeap[0]['H'] if len(maxHeap) > 0 else 0
-            if not skyline or height != skyline[-1]['H']:
-                skyline.append({'X':x, 'H':height})
-        return [[i['X'], i['H']] for i in skyline]
+        """
+        :type buildings: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        events = sorted([(L, -H, R) for L, R, H in buildings] + list(set((R, 0, None) for _, R, _ in buildings)))
+        res, hp = [[0, 0]], [(0, float("inf"))]
+        for x, negH, R in events:
+            while x >= hp[0][1]:
+                heapq.heappop(hp)
+            if negH:
+                heapq.heappush(hp, (negH, R))
+            if res[-1][1] + hp[0][0]:
+                res += [x, -hp[0][0]],
+        return res[1:] + [sys.version]
