ce9f3aab1f726bd00f3dbe6769875f4cc354ef07
Chao <chaoren.hust@gmail.com>
2016-01-17 03:38:10 -0800

    315

---
+++ 315_Count_of_Smaller_Numbers_After_Self.py
@@ -0,0 +1,26 @@
+# 2016-01-17  16 tests, 160 ms
+class Solution(object):
+    def countSmaller(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        rank, N, res = {val: i + 1 for i, val in enumerate(sorted(nums))}, len(nums), []
+        BITree = [0] * (N + 1)
+
+        def update(i):
+            while i <= N:
+                BITree[i] += 1
+                i += (i & -i)
+
+        def getSum(i):
+            s = 0
+            while i:
+                s += BITree[i]
+                i -= (i & -i)
+            return s
+
+        for x in reversed(nums):
+            res += getSum(rank[x] - 1),
+            update(rank[x])
+        return res[::-1]
