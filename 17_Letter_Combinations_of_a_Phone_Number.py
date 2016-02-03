b5331f101363b923f3466f76819ab50c4268f4c6
Chao <chaoren.hust@gmail.com>
2016-02-03 00:02:46 -0800

    17

--- 17_Letter_Combinations_of_a_Phone_Number.py
+++ 17_Letter_Combinations_of_a_Phone_Number.py
@@ -1,12 +1,11 @@
-# 2015-07-29  Runtime: 44 ms
-class Solution:
-    # @param {string} digits
-    # @return {string[]}
+# 2016-02-03  Runtime: 40 ms
+class Solution(object):
     def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
         if not digits: return []
-        key = {'0':' ', '2':'abc', '3':'def', '4':'ghi',
-               '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
-        result = list(key[digits[0]])
-        for i in xrange(1, len(digits)):
-            result = [x + y for x in result for y in key[digits[i]]]
-        return result  
+        mapping = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl',
+                   '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
+        return reduce(lambda resultList, digit: [x + y for x in resultList for y in mapping[digit]], digits, [''])
