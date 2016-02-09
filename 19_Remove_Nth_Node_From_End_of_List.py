2ffca1c41be1b565b232e7f2af5df6a297dc1f10
Chao <chaoren.hust@gmail.com>
2016-02-09 09:02:27 -0800

    19

--- 19_Remove_Nth_Node_From_End_of_List.py
+++ 19_Remove_Nth_Node_From_End_of_List.py
@@ -1,21 +1,22 @@
-# 2015-06-04  Runtime: 56 ms
+# 2016-02-09  Runtime: 52 ms
 
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
-    # @param {ListNode} head
-    # @param {integer} n
-    # @return {ListNode}
+class Solution(object):
     def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
         dummyHead = ListNode(0)
-        dummyHead.next = head
-        p1_pre, p1, p2 = dummyHead, head, head
-        for i in xrange(n): p2 = p2.next
-        while p2:
-            p1_pre, p1, p2 = p1_pre.next, p1.next, p2.next
-        p1_pre.next = p1.next
+        p1, p2, dummyHead.next = dummyHead, dummyHead, head
+        for i in range(n): p2 = p2.next
+        while p2.next:
+            p1, p2 = p1.next, p2.next
+        p1.next = p1.next.next
         return dummyHead.next
