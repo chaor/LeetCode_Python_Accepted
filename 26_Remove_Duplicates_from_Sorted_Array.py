0afeeef9b3767610358b3e831c8b25a5e5b57e29
Chao <chaoren.hust@gmail.com>
2016-02-09 23:13:43 -0800

    26

--- 26_Remove_Duplicates_from_Sorted_Array.py
+++ 26_Remove_Duplicates_from_Sorted_Array.py
@@ -1,12 +1,14 @@
-# 2015-06-04  Runtime: 100 ms
-class Solution:
-    # @param {integer[]} nums
-    # @return {integer}
+# 2016-02-09  Runtime: 96 ms
+class Solution(object):
     def removeDuplicates(self, nums):
-        if len(nums) < 2: return len(nums)
-        slow = 1
-        for fast in xrange(1, len(nums)):
-            if nums[fast] != nums[fast - 1]:
-                nums[slow] = nums[fast]
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        length, slow = len(nums), 0
+        if length <= 1: return length
+        for fast in range(length):
+            if nums[fast] != nums[slow]:
                 slow += 1
-        return slow
+                nums[slow] = nums[fast]
+        return slow + 1
