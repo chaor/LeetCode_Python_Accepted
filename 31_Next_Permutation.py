f3e8c6ea4ce1b00110a6f2eac735fc2cc52ce3f3
Chao <chaoren.hust@gmail.com>
2016-02-21 17:53:00 -0800

    31

--- 31_Next_Permutation.py
+++ 31_Next_Permutation.py
@@ -1,20 +1,21 @@
-# 2015-08-09  Runtime: 60 ms
-class Solution:
-    # @param {integer[]} nums
-    # @return {void} Do not return anything, modify nums in-place instead.
+# 2016-02-21  Runtime: 60 ms
+class Solution(object):
     def nextPermutation(self, nums):
-        # Find the highest index i such that nums[i] < nums[i+1], call nums[i] violated number
-        i = len(nums) - 2
-        while i >= 0:
-            if nums[i] < nums[i + 1]: break
-            i -= 1
-        # If no such index exists, the permutation is the last permutation.
-        if i == -1:
-            nums.reverse()
-            return
-        # reverse all numbers after violated number
-        nums[i + 1:] = nums[len(nums) - 1: i: -1]
-        # find the 1st number larger than the violated number
-        j = bisect.bisect_right(nums, nums[i], lo = i + 1, hi = len(nums))
-        # swap these two numbers
-        nums[i], nums[j] = nums[j], nums[i]
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        # from right to left find longest increasing sequence
+        i = len(nums) - 1
+        while i > 0 and nums[i - 1] >= nums[i]: i -= 1
+        # swap nums[i - 1] and min(number in nums[i...end] which is larger than nums[i - 1])
+        # consider it as a carry to nums[i - 1]
+        if i > 0:
+            j, pre = len(nums) - 1, nums[i - 1]
+            while j >= i and nums[j] <= pre: j -= 1
+            nums[i - 1], nums[j] = nums[j], nums[i - 1]
+        # reverse nums[i...end]
+        k = len(nums) - 1
+        while i < k:
+            nums[i], nums[k] = nums[k], nums[i]
+            i, k = i + 1, k - 1
