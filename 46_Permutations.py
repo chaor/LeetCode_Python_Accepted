aaedfc35aa5396fe4f5c716e382d7550c24d3157
Chao <chaoren.hust@gmail.com>
2016-03-02 09:48:47 -0800

    46

--- 46_Permutations.py
+++ 46_Permutations.py
@@ -1,20 +1,9 @@
-# 2015-04-15 Runtime: 99 ms  
-
-class Solution:
-    # @param num, a list of integer
-    # @return a list of lists of integers
-    def permute(self, num):
-        self.res, self.num = [], num
-        self.recursive(0)
-        return self.res
-        
-    def recursive(self, beginPos):
-        if beginPos >= len(self.num):
-            # # put the copy of self.num into self.res, otherwise everything in self.res will be the same
-            self.res.append(self.num[:]) 
-            return
-        # fix self.num[0 ... beginPos-1], permute self.num[beginPos ... end]
-        for i in xrange(beginPos, len(self.num)):
-            self.num[i], self.num[beginPos] = self.num[beginPos], self.num[i]
-            self.recursive(beginPos + 1)
-            self.num[beginPos], self.num[i] = self.num[i], self.num[beginPos]
+# 2015-04-15  25 tests, 84 ms
+# thanks to https://leetcode.com/discuss/42550/one-liners-in-python
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return [[n] + p for i, n in enumerate(nums) for p in self.permute(nums[:i] + nums[i+1:])] or [[]]
