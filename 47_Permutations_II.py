4fcdb7fa944ee773bee66b53e9a33f0a6d01f74a
Chao <chaoren.hust@gmail.com>
2016-03-03 09:05:52 -0800

    47

--- 47_Permutations_II.py
+++ 47_Permutations_II.py
@@ -1,15 +1,8 @@
-# 2015-08-15  Runtime: 132 ms
-
-class Solution:
-    # @param {integer[]} nums
-    # @return {integer[][]}
-    def permuteUnique(self, A):
-        if len(A) == 1: return [A]
-        A.sort()
-        res, prev = [], None
-        for i in xrange(len(A)):
-            if A[i] == prev: continue
-            prev = A[i]
-            for x in self.permuteUnique(A[:i] + A[i + 1:]):
-                res.append([A[i]] + x)
-        return res
+# 2016-03-03  30 tests, 128 ms
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return [[n] + p for n in set(nums) for p in self.permuteUnique(nums[:nums.index(n)] + nums[nums.index(n) + 1:])] or [[]]
