52e140f110f1c2689efe42f2a4bc8e9d677ca216
Chao <chaoren.hust@gmail.com>
2016-03-07 09:18:52 -0800

    43

--- 43_Multiply_Strings.py
+++ 43_Multiply_Strings.py
@@ -1,15 +1,17 @@
-# 2015-08-15  Runtime: 240 ms
-class Solution:
-    # @param {string} num1
-    # @param {string} num2
-    # @return {string}
+# 2016-03-07  311 tests, 180 ms
+class Solution(object):
     def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
         if num1 == '0' or num2 == '0': return '0'
-        num1, num2 = num1[::-1], num2[::-1]
-        res = [0 for i in xrange(len(num1) + len(num2))]
-        for i in xrange(len(num1)):
-            for j in xrange(len(num2)):
-                res[i + j] += (ord(num1[i]) - ord('0')) * (ord(num2[j]) - ord('0'))
+        a, b, len_a, len_b = [ord(i) - 48 for i in num1[::-1]], [ord(j) - 48 for j in num2[::-1]], len(num1), len(num2)
+        res = [0] * (len_a + len_b)
+        for i in xrange(len_a):
+            for j in xrange(len_b):
+                res[i + j] += a[i] * b[j]
                 res[i + j + 1] += res[i + j] / 10
                 res[i + j] %= 10
-        return ''.join([chr(ord('0') + x) for x in res])[::-1].lstrip('0')
+        return ''.join([chr(48 + x) for x in res])[::-1].lstrip('0')
