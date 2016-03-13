2a1799a1e609ecf7a4414d7510e33d3014aaf248
Chao <chaoren.hust@gmail.com>
2016-03-13 09:41:41 -0700

    8

--- 8_String_to_Integer_atoi.py
+++ 8_String_to_Integer_atoi.py
@@ -1,18 +1,15 @@
-# 2015-05-07  Runtime: 79 ms
-class Solution:
-    # @param {string} str
-    # @return {integer}
-    def myAtoi(self, str):
-        maxDiv10 = 214748364  # Integer.MAX_VALUE / 10 (Java)
-        i, n = 0, len(str)
-        while i < n and str[i] == ' ': i += 1
-        sign = 1
-        if i < n and str[i] == '+': i += 1
-        elif i < n and str[i] == '-': sign, i = -1, i + 1
-        num = 0
-        while i < n and '0' <= str[i] <= '9':
-            if num > maxDiv10 or (num == maxDiv10 and int(str[i]) >= 8):
-                return 2147483647 if sign == 1 else -2147483648
-            num = num * 10 + int(str[i])
-            i += 1
-        return sign * num
+# 2016-03-13  Runtime: 68 ms
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type str: str
+        :rtype: int
+        """
+        try:
+            s = s.lstrip() + '$' # remove leading spaces and append an end mark
+            for i, ch in enumerate(s):
+                if not (ch in '+-' or '0' <= ch <= '9'):
+                    result = int(s[:i])
+                    return -2 ** 31 if result < -2 ** 31 else 2 ** 31 - 1 if result > 2 ** 31 - 1 else result
+        except:
+            return 0
