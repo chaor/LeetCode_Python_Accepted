fe63b95afb8cea5b5b94d2d6e1b027e899d04f57
Chao <chaoren.hust@gmail.com>
2016-03-13 14:45:31 -0700

    44

--- 44_Wildcard_Matching.py
+++ 44_Wildcard_Matching.py
@@ -1,26 +1,24 @@
-# 2015-06-15  Runtime: 168 ms
-class Solution:
-    # @param {string} s
-    # @param {string} p
-    # @return {boolean}
+# 2016-03-13  Runtime: 1801 tests, 120 ms
+class Solution(object):
     def isMatch(self, s, p):
-        i, j, starCanMatch, lastStarPos = 0, 0, 0, -1
-        while i < len(s):
-            if j < len(p) and (p[j] == '?' or s[i] == p[j]):
-                i += 1
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        i, j, star_match_pos, last_star_pos, len_s, len_p = 0, 0, 0, -1, len(s), len(p)
+        while i < len_s:
+            if j < len_p and p[j] in (s[i], '?'):
+                i, j = i + 1, j + 1
+            # when meet a '*', first assume it will match 0 character in s
+            elif j < len_p and p[j] == '*':
+                star_match_pos, last_star_pos = i, j
                 j += 1
-                continue
-            # when meet a '*', first assume it will match 0 character
-            if j < len(p) and p[j] == '*':
-                lastStarPos, starCanMatch = j, i 
-                j += 1
-                continue
             # now p[j] is not ?, not *, can't match s[i], we can only use the last '*'
-            if lastStarPos > -1:
-                i = starCanMatch + 1
-                starCanMatch += 1
-                j = lastStarPos + 1
-                continue
-            return False
-        while j < len(p) and p[j] == '*': j += 1
-        return j == len(p)
+            elif last_star_pos > -1:
+                i, star_match_pos = star_match_pos + 1, star_match_pos + 1
+                j = last_star_pos + 1
+            else:
+                return False
+        while j < len_p and p[j] == '*': j += 1
+        return j == len_p
