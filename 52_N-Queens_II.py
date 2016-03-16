82df40ef1b1eb429390c48e0a29ef4c561c9e145
Chao <chaoren.hust@gmail.com>
2016-03-16 09:54:17 -0700

    52

--- 52_N-Queens_II.py
+++ 52_N-Queens_II.py
@@ -1,25 +1,19 @@
-# 2015-06-17  Runtime: 112 ms
-class Solution:
-    # @param {integer} n
-    # @return {integer}
+# 2016-03-16,  9 tests, 132 ms
+class Solution(object):
     def totalNQueens(self, n):
-        # e.g. n = 4, a board [1, 3, 0, 2] means queens are at [0][1], [1][3], [2][0], [3][2]
-        self.count, self.board, self.n = 0, [-1 for i in xrange(n)], n
-        self.solve(0)
-        return self.count
-        
-    def solve(self, rowNum):
-        if rowNum == self.n:
-            self.count += 1
-            return
-        availableColumn = [i for i in xrange(self.n)]
-        # for the new row, make sure there's no column conflict
-        for i in xrange(rowNum): availableColumn.remove(self.board[i])
-        # for the new row, make sure there's no diagonal conflict
-        for col in availableColumn[:]: # make a copy of availableColumn because we will delete some elements in iteration
-            for i in xrange(rowNum):
-                if col in availableColumn and abs(i - rowNum) == abs(self.board[i] - col): availableColumn.remove(col)
-        # now all values in availableColumn is OK to put a Queen
-        for col in availableColumn:
-            self.board[rowNum] = col
-            self.solve(rowNum + 1)
+        """
+        :type n: int
+        :rtype: int
+        """
+        def dfs(board, row):
+            if row == n: return 1
+            count = 0
+            for x in set_n - set(board):
+                if all(row - i != abs(x - y) for i, y in enumerate(board[:row])):
+                    board[row] = x
+                    count += dfs(board, row + 1)
+                    board[row] = '.'
+            return count
+
+        set_n = {i for i in xrange(n)}
+        return dfs(['.'] * n, 0)
