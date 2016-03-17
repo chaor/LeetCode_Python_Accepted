853627d44406272d741722e89e169ba118773d9b
Chao <chaoren.hust@gmail.com>
2016-03-17 09:59:56 -0700

    338, double, double, double

--- 338_Counting_Bits.py
+++ 338_Counting_Bits.py
@@ -8,3 +8,14 @@ class Solution(object):
         result = [0]
         while len(result) < num + 1: result += [x + 1 for x in result]
         return result[:num + 1]
+
+# 16 tests, 812 ms
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        result = [0] * (num + 1)
+        for i in xrange(1, num + 1): result[i] = result[i / 2] + i % 2
+        return result
