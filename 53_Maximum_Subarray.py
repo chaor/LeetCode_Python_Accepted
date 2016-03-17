dc4b4b02f4562b0984afcc07f6baf4df9548d2cd
Chao <chaoren.hust@gmail.com>
2016-03-16 23:00:36 -0700

    53

--- 53_Maximum_Subarray.py
+++ 53_Maximum_Subarray.py
@@ -1,14 +1,14 @@
-# 2015-05-27, dynamic programming, runtime: 68 ms
-class Solution:
-    # @param {integer[]} nums
-    # @return {integer}
+# 2016-03-16, 201 tests, 56 ms
+class Solution(object):
     def maxSubArray(self, nums):
-        # let f(k) = max sum of subarray ending at index k, f(k) = max(f(k-1) + A[k], A[k])
-        fk, Max = nums[0], nums[0]
-        for i in xrange(1, len(nums)):
-            fk = max(fk + nums[i], nums[i])
-            Max = max(fk, Max)
-        return Max
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [float('-inf')] + [None] * len(nums)
+        for i, x in enumerate(nums):
+            dp[i + 1] = max(dp[i] + x, x)
+        return max(dp)
 
 
 # 2015-05-27, divide and conquer, runtime: 164 ms
@@ -19,7 +19,7 @@ class Solution:
         # divide and conquer, the maximum subarray could either contain the middle element or not
         self.nums = nums
         return self.divideAndConquer(0, len(nums) - 1)
-        
+
     def divideAndConquer(self, L, R):
         if L > R: return -10**10  # infinitesimal
         M = (L + R) / 2
