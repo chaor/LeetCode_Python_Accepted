fc74cce0890e8e70bf4986d88b759643e331dc90
Chao <chaoren.hust@gmail.com>
2016-03-22 01:59:50 -0700

    59, step pattern is n, n-1, n-1, n-2, n-2 ..., 2, 2, 1, 1

--- 59_Spiral_Matrix_II.py
+++ 59_Spiral_Matrix_II.py
@@ -1,15 +1,14 @@
-# 2015-08-30  Runtime: 64 ms
-# thanks to https://leetcode.com/discuss/46720/4-9-lines-python-solutions
+# 2016-03-22  21 tests, 48 ms
 class Solution(object):
     def generateMatrix(self, n):
         """
         :type n: int
         :rtype: List[List[int]]
         """
-        i, j, di, dj, m = 0, 0, 0, 1, [[0] * n for i in xrange(n)]
-        for x in xrange(1, n * n + 1):
-            m[i][j] = x
-            if m[(i + di) % n][(j + dj) % n]: # if there's already a number in m, turn right
-                di, dj = dj, -di
-            i, j = i + di, j + dj
-        return m
+        mat, x, y, dx, dy, number = [[0] * n for i in xrange(n)], -1, 0, 1, 0, 0
+        for step in [i / 2 for i in xrange(2 * n, 1, -1)]:
+            for j in xrange(step):
+                x, y, number = x + dx, y + dy, number + 1
+                mat[y][x] = number
+            dx, dy = -dy, dx # turn right
+        return mat
