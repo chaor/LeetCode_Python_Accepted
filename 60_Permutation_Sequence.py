6a94785fad2708a02e80a8ebee95a7d71195049e
Chao <chaoren.hust@gmail.com>
2016-03-26 17:56:52 -0700

    60, get candidate[index] by curr_k / (n - 1)!, curr_k / (n - 2)!, ... curr_k / 0!

--- 60_Permutation_Sequence.py
+++ 60_Permutation_Sequence.py
@@ -1,4 +1,4 @@
-# 2015-08-30  Runtime: 44 ms  
+# 2016-03-26  200 tests, 56 ms
 class Solution(object):
     def getPermutation(self, n, k):
         """
@@ -6,9 +6,10 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        k, res, fac, char = k - 1, '', math.factorial(n - 1), ['1', '2', '3', '4', '5', '6', '7', '8', '9'][:n]
-        for i in xrange(n - 1, -1, -1):
-            idx, k = divmod(k, fac)
-            if i: fac /= i
-            res += char.pop(idx)
-        return res
+        k, fac, candidate, result = k - 1, math.factorial(n), ['1', '2', '3', '4', '5', '6', '7', '8', '9'][:n], []
+        for i in xrange(n, 0, -1):
+            fac /= i
+            index, k = divmod(k, fac)
+            result.append(candidate[index])
+            del candidate[index]
+        return ''.join(result)
