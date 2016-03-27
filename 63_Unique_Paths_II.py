74dbe67e8a7cd594dab9f8ed0161cbbbc7dbf7b6
Chao <chaoren.hust@gmail.com>
2016-03-26 20:24:01 -0700

    63, initialize first row and first column, then dp

--- 63_Unique_Paths_II.py
+++ 63_Unique_Paths_II.py
@@ -1,14 +1,22 @@
-# 2015-05-26  Runtime: 56 ms
-
-class Solution:
-    # @param {integer[][]} obstacleGrid
-    # @return {integer}
+# 2016-03-26   43 tests, 52 ms
+class Solution(object):
     def uniquePathsWithObstacles(self, obstacleGrid):
-        rows, cols = len(obstacleGrid), len(obstacleGrid[0])
-        matrix = [[0 for i in xrange(cols + 1)] for j in xrange(rows + 1)]
-        # we use this to fill matrix[row - 1][cols - 1] with 1 in the first inner loop
-        matrix[rows - 1][cols] = 1 
-        for i in reversed(xrange(rows)):
-            for j in reversed(xrange(cols)):
-                matrix[i][j] = 0 if obstacleGrid[i][j] == 1 else matrix[i + 1][j] + matrix[i][j + 1]
-        return matrix[0][0]
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        # initialization
+        dp = [[0] * n for i in xrange(m)]
+        for i in xrange(m):
+            if obstacleGrid[i][0] == 1: break
+            dp[i][0] = 1
+        for j in xrange(n):
+            if obstacleGrid[0][j] == 1: break
+            dp[0][j] = 1
+        # dp
+        for i in xrange(1, m):
+            for j in xrange(1, n):
+                if not obstacleGrid[i][j]:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m - 1][n - 1]
